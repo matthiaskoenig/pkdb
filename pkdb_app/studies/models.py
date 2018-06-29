4af00b08500ffa4376f277a22ee35fae7ae29ce3
Matthias Koenig <konigmatt@googlemail.com>
2018-06-29 16:43:43 +0200

    status before refactoring

--- pkdb_app/studies/models.py
+++ pkdb_app/studies/models.py
@@ -14,6 +14,7 @@ class Author(models.Model):
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
 
+
 class Study(Commentable, Describable, models.Model):
     """ Single clinical study.
 
