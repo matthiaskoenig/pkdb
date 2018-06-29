0e96d8b2c293dd2c5f45414125acca5cffd76fd9
Matthias Koenig <konigmatt@googlemail.com>
2018-06-29 17:26:36 +0200

    working on model

--- pkdb_app/choices.py
+++ pkdb_app/choices.py
@@ -1,20 +1,20 @@
 
 INTERVENTION_CHOICES = (
-    (1, "Else"),
-    (2, "Dynamic Single"),
-    (3, "Dynamic Multiple"),
+    (1, "Other"),
+    (2, "Dynamic Individual"),
+    (3, "Dynamic Group"),
     (4, "Static Single"),
     (5, "Static Multiple"),
     )
 
-SPECIE_CHOICES = (
-    (1, "Else"),
+SPECIES_CHOICES = (
+    (1, "Other"),
     (2, "Homo Sapiens"),
     )
 
 CHARATERISTIC_CHOICES = (
-    (1,"Else"),
-    (2,"Antropometrie"),
-    (3,"Life Style"),
-    (4,"Genetics")
+    (1, "Other"),
+    (2, "Antropometrie"),
+    (3, "Life Style"),
+    (4, "Genetics")
 )
