d71f5f9074928382a70b4c023b590f958203e243
Matthias Koenig <konigmatt@googlemail.com>
2018-07-02 18:43:08 +0200

    updated models

--- pkdb_app/categoricals.py
+++ pkdb_app/categoricals.py
@@ -1,124 +1,74 @@
 """
+To be easily extendable we do not hardcode information about characteristics in classes,
+but define in some custom data structure which is referenced.
+
+
 Model choices and more specifically possible categories of subject characteristics.
+
+In the future the relationship between the characteristics could be implemented via
+an ontology which represents the relationship between the differnent values.
 """
-from enum import Enum, IntEnum
-from itertools import chain
-#####################
-#Abstract Catergories
 
-class ChoiceEnum(Enum):
+# TODO: How to handle the genetic information? Genetic variants?
 
-    @classmethod
-    def choices(cls):
-        return tuple((x.name, x.value) for x in cls)
+from collections import namedtuple
+CharacteristicType = namedtuple("CharacteristicType", ["value", "class", "dtype", "choices"])
+UnitType = namedtuple("UnitType", ["name"])
 
-BOOLEAN_CHOICES = (
-    (0,"Yes"),
-    (1,"No")
-)
-
-NULL_BOOLEAN_CHOICE = BOOLEAN_CHOICES + (2,"NULL")
-
-
-#####################
-#Study Choices
-KEY_WORD_CHOICES = ()
-#####################
-#Characteristics
-
-class BodyParams(ChoiceEnum):
-    Height = "height"
-    BODYWEIGHT = "bodyweight"
-
-class Sex(ChoiceEnum):
-    MALE = 'male'
-    FEMALE = 'female'
-
-class Species(ChoiceEnum):
-    HOMOSAPIEN = "Home Sapiens"
-
-class Ethnicitiy(ChoiceEnum):
-    ASIAN = "asian"
-    CAUCASIAN = "caucasian"
-    AFRICAN = "african"
-    AFROAMERICAN = "afroamerican"
-
-class Health(ChoiceEnum):
-    HEALTHY = "healthy"
-    NONHEALTHY = "non-healthy"
-
-class Smoking(ChoiceEnum):
-    SMOKING = "smoking"
-    NONSMOKING = "non-smoking"
-
-characteristics_dict = {"sex":Sex,
-                   "ethnicity":Ethnicitiy,
-                   "health":Health,
-                   "smoking":Smoking,
-                   "body_params": BodyParams,
-                   "species":Species}
-
-
-characteristics_types = {}
-for k,v in characteristics_dict.items():
-    for vv in v:
-        characteristics_types[vv.value] = k
-
-All_Characteristics = ChoiceEnum("All Characteristics", [(i.name, i.value) for i in chain(*characteristics_dict.values)])
+# TODO: lookup units package and proper units handling (units conversion, default units, ...)
+UNIT_DATA = [
+    UnitType('-'),
+    UnitType('cm'), UnitType('m'),
+    UnitType('kg'),
+    UnitType('y'),
+    UnitType('kg/m^2'),
+]
+UNITS_CHOICES = [(utype.name, utype.name) for utype in UNIT_DATA]
 
 
 
+# class, value, dtype (numeric, boolean, categorial), choices
+# dates?
+# How to store NA? Is this necessary?
+#   numeric: NA, None
+#   boolean: NA, None
+#   categorial: NA, None
+
+CHARACTERISTIC_DATA = [
+    CharacteristicType('height', 'BodyParameter', 'numeric', None),
+    CharacteristicType('body weight', 'BodyParameter', 'numeric', None),
+    CharacteristicType('age', 'BodyParameter', 'numeric', None),
+    CharacteristicType('sex', 'BodyParameter', 'categorial', ["M", "F"]),
+    CharacteristicType('bmi', 'BodyParameter', 'numeric', None),
+    CharacteristicType('waist circumference', 'BodyParameter', 'numeric', None),
+
+    CharacteristicType('ethnicity', 'Ethnicity', 'categorial', ["african", "afroamerican", "asian", "caucasian"]),
+
+    # exists as yes/no or as amount
+    CharacteristicType('healthy', "Health status", "boolean", None),
+    CharacteristicType('disease', "Disease", "categorial", ["cirrhosis"]),
+
+    CharacteristicType('smoking', 'Lifestyle', 'boolean', ["Y", "N"]),
+    CharacteristicType('smoking_amount', 'Lifestyle', 'numeric', None),
+    CharacteristicType('alcohol', 'Lifestyle', 'boolean', ["Y", "N"]),
+    CharacteristicType('alcohol_amount', 'Lifestyle', 'numeric', None),
+
+    CharacteristicType('oral contraceptives', 'Contraceptives', 'boolean', ["Y", "N"]),
+
+    CharacteristicType('medication', 'Medication', 'categorial', ["ibuprofen", "paracetamol", "aspirin"]),  # ? dosing
+    CharacteristicType('species', 'Species', 'categorial', ["Homo sapiens"]),
+]
+
+CHARACTERISTIC_DICT = {item.value: item for item in CHARACTERISTIC_DATA}
+CHARACTERISTIC_CHOICES = [(ctype.value, ctype.value) for ctype in CHARACTERISTIC_DATA]
 
 
-
-INTERVENTION_CHOICES = (
+'''
+DATA_CHOICES = (
      (1, "Other"),
      (2, "Dynamic Individual"),
      (3, "Dynamic Group"),
      (4, "Static Single"),
      (5, "Static Multiple"),
      )
-#
-# SPECIES_CHOICES = (
-#     (1, "Other"),
-#     (2, "Homo Sapiens"),
-#     )
-#
-# CHARATERISTIC_CHOICES = (
-#     (1, "Other"),
-#     (2, "Antropometrie"),
-#     (3, "Life Style"),
-#     (4, "Genetics")
-# )
-# #####################
-# # Subject categories
-# CHARATERISTIC_CATEGORIES = (
-#     #(1, "Other"),
-#     (2,"Health"),
-#     (3,"Sex"),
-#     (4,"Smoking"),
-#     (5,"Ethnicity"),
-# )
-#
-# SEX_CATEGORIES = (
-#     (1,"Male"),
-#     (2,"Female"),
-# )
-#
-# ETHNICITY_CATEGORIES = (
-#     (1,"Asian"),
-#     (2,"Afroamerican"),
-#     (3,"American"),
-#     (4,"Caucasian"),
-# )
-#
-# HEALTH_CATEGORIES = BOOLEAN_CHOICES
-# SMOKING_CATEGORIES = BOOLEAN_CHOICES
-#
-#
-# SUBJECT_CATEGORIES = {"health":HEALTH_CATEGORIES,
-#                       "smoking":SMOKING_CATEGORIES,
-#                       "sex": SEX_CATEGORIES,
-#                       "ethnicity": ETHNICITY_CATEGORIES,
-#                       }
-
+'''
