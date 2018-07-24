0cbd4e9422e629477ee506f0771d7f8bf7fb9260
Jan <janekg89@hotmail.de>
2018-07-24 14:46:10 +0200

    working on schemas

---
+++ pkdb_app/data_management/schemas.py
@@ -0,0 +1,16 @@
+
+reference_schema = {
+    "properties": {
+        "pmid": {"type": "number"},
+        "name": {"type": "string"},
+        "sid": {"type": ["number", "string"]},
+        "date": {"type": "string"},
+        "journal": {"type": "string"},
+        "title": {"type": "string"},
+        "abstract": {"type": "string"},
+        "authors": {"type": "array"},
+        "doi": {"type": "string"}
+
+    }
+
+}
