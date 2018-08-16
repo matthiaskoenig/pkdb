f434264ab472400889c8cb783c99d7cf73711b67
Matthias Koenig <konigmatt@googlemail.com>
2018-08-16 16:09:13 +0200

    refactored fill_database

--- pkdb_app/data_management/fill_database.py
+++ pkdb_app/data_management/fill_database.py
@@ -40,9 +40,9 @@ from pkdb_app.data_management.create_reference_caffeine import REFERENCESMASTERP
 PASSWORD = "test"
 USERS = [
     {"username": "janekg", "first_name": "Jan", "last_name": "Grzegorzewski", "email": "Janekg89@hotmail.de",
-          "password": PASSWORD},
+     "password": PASSWORD},
     {"username": "mkoenig", "first_name": "Matthias", "last_name": "König", "email": "konigmatt@googlemail.com",
-           "password": PASSWORD}
+     "password": PASSWORD}
 ]
 
 # -----------------------------
@@ -123,43 +123,12 @@ def read_study_json(path):
             "study_path": path}
 
 
-
-
-def upload_reference(json_reference):
-    ok = True
-    validate(json_reference["json"],reference_schema)
-    #client.action(document, ["references", "create"], params=json_reference["json"])
-    response = requests.post(f'http://0.0.0.0:8000/api/v1/references/', json=json_reference["json"])
-    if not response.status_code == 201:
-        print(json_reference["json"]["name"], response.text)
-        ok = False
-
-
-    with open(json_reference["pdf"],'rb') as f:
-        response = requests.patch(f'http://0.0.0.0:8000/api/v1/references/{json_reference["json"]["sid"]}/', files={"pdf":f})
-
-    if not response.status_code == 200:
-        print(json_reference["json"]["name"], response.text)
-        ok = False
-
-    return ok
-
-
-
-def fill_files(file_path):
-    data_dict = {}
-    head, sid = os.path.split(file_path)
-    study_dir = os.path.join(head,sid)
-    for root, dirs, files in os.walk(study_dir, topdown=False):
-        files = set(files) - set(['reference.json', 'study.json', f'{sid}.pdf'])
-        for file in files:
-            file_path = os.path.join(root, file)
-            with open(file_path,'rb') as f:
-                response = requests.post(f'http://0.0.0.0:8000/api/v1/datafiles/', files={"file": f})
-            data_dict[file] = response.json()["id"]
-    return data_dict
+# -------------------------------
+# Helpers
+# -------------------------------
 
 def recursive_iter(obj, keys=()):
+    """ Creates dictionary with key:object from nested JSON data structure. """
     if isinstance(obj, dict):
         for k, v in obj.items():
             yield from recursive_iter(v, keys + (k,))
@@ -169,27 +138,77 @@ def recursive_iter(obj, keys=()):
     else:
         yield keys, obj
 
+
 def set_keys(d, value, *keys):
+    """ Changes keys in nested dictionary. """
     for key in keys[:-1]:
         d = d[key]
     d[keys[-1]] = value
-def pop_comment(d, *keys):
 
+
+def pop_comment(d, *keys):
+    """ Pops comment in nested dictionary. """
     for key in keys[:-1]:
 
-        if  key == "comments":
+        if key == "comments":
             d.pop("comments")
             return
 
         d = d[key]
 
 
+# -------------------------------
+# Upload JSON in database
+# -------------------------------
+API_URL = "http://0.0.0.0:8000/api/v1"
+
+
+def upload_files(file_path):
+    """ Uploads all files in directory of given file.
+
+    :param file_path:
+    :return: dict with all keys for files
+    """
+    data_dict = {}
+    head, sid = os.path.split(file_path)
+    study_dir = os.path.join(head, sid)
+    for root, dirs, files in os.walk(study_dir, topdown=False):
+        files = set(files) - set(['reference.json', 'study.json', f'{sid}.pdf'])
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'rb') as f:
+                response = requests.post(f'{API_URL}/datafiles/', files={"file": f})
+            data_dict[file] = response.json()["id"]
+    return data_dict
+
+
+def upload_reference(json_reference):
+    """ Uploads reference JSON. """
+    success = True
+    validate(json_reference["json"], reference_schema)
+
+    # post
+    response = requests.post(f'{API_URL}/references/', json=json_reference["json"])
+    if not response.status_code == 201:
+        print(json_reference["json"]["name"], response.text)
+        success = False
+
+    # patch
+    with open(json_reference["pdf"], 'rb') as f:
+        response = requests.patch(f'{API_URL}/references/{json_reference["json"]["sid"]}/', files={"pdf": f})
+
+    if not response.status_code == 200:
+        print(json_reference["json"]["name"], response.text)
+        success = False
+
+    return success
 
 
 def upload_study(json_study):
-    ok = True
+    """ Uploads study JSON. """
+    success = True
     study_dir = os.path.dirname(json_study["study_path"])
-    file_dict = fill_files(study_dir)
+    file_dict = upload_files(study_dir)
     comments = []
     for keys, item in recursive_iter(json_study):
         if item in file_dict.keys():
@@ -198,12 +217,9 @@ def upload_study(json_study):
             comments.append(keys)
 
     for comment in comments:
-        pop_comment(json_study,*comment)
-
-
+        pop_comment(json_study, *comment)
 
     study_partial = {}
-
     study_partial["sid"] = json_study["json"]["sid"]
     study_partial["name"] = json_study["json"]["name"]
     study_partial["pkdb_version"] = json_study["json"]["pkdb_version"]
@@ -213,44 +229,38 @@ def upload_study(json_study):
     study_partial["curators"] = json_study["json"]["curators"]
     study_partial["creator"] = json_study["json"]["creator"]
     study_partial["files"] = list(file_dict.values())
-    #study_partial["bla"] = "bla"
 
-
-
-
-
-    #client.action(document, ["studies", "create"], params=study_partial)
-    response = requests.post(f'http://0.0.0.0:8000/api/v1/studies/',
-                              json=study_partial)
+    # post
+    response = requests.post(f'{API_URL}/studies/', json=study_partial)
     if not response.status_code == 201:
-        print(json_study["json"]["name"],response.text)
-        ok = False
-
-
-
+        print(json_study["json"]["name"], response.text)
+        success = False
 
     study_partial2 = {}
     study_partial2["groupset"] = json_study["json"]["groupset"]
     study_partial2["interventionset"] = json_study["json"]["interventionset"]
     study_partial2["individualset"] = json_study["json"].get("individualset", None)
 
-
-    response = requests.patch(f'http://0.0.0.0:8000/api/v1/studies/{json_study["json"]["sid"]}/', json = study_partial2)
+    # patch
+    response = requests.patch(f'{API_URL}/studies/{json_study["json"]["sid"]}/', json=study_partial2)
     if not response.status_code == 200:
         print(json_study["json"]["name"], response.text)
-        ok = False
+        success = False
 
+    # patch
     if "outputset" in json_study["json"].keys():
-        response = requests.patch(f'http://0.0.0.0:8000/api/v1/studies/{json_study["json"]["sid"]}/', json = {"outputset": json_study["json"].get("outputset")})
+        response = requests.patch(f'{API_URL}/studies/{json_study["json"]["sid"]}/',
+                                  json={"outputset": json_study["json"].get("outputset")})
         if not response.status_code == 200:
-            print(json_study["json"]["name"],response.text)
-            ok = False
+            print(json_study["json"]["name"], response.text)
+            success = False
 
-    return ok
-
-    #study_partial["outputset"] = json_study["json"].get("outputset",None)
+    return success
 
 
+# -------------------------------
+# Bonobo
+# -------------------------------
 def get_graph_references(**options):
     graph = bonobo.Graph()
     # add studies
@@ -260,6 +270,7 @@ def get_graph_references(**options):
         upload_reference,
     )
     return graph
+
 
 def get_graph_study(**options):
     graph = bonobo.Graph()
@@ -276,6 +287,7 @@ def get_services(**options):
     return {}
 
 
+# -------------------------------------------------------------------------------
 if __name__ == '__main__':
     client = coreapi.Client()
     document = client.get("http://0.0.0.0:8000/")
