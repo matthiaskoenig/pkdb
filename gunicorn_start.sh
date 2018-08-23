d1751f9e4435c93b922d05ef364961a6a3f7cb0a
Matthias Koenig <konigmatt@googlemail.com>
2018-08-23 18:21:39 +0200

    gunicorn_start.sh fixed

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 NAME="pkdb"                                        # Name of the application (*)
 DJANGODIR=/var/git/pkdb                            # Django project directory (*)
 SOCKFILE=/var/git/pkdb/run/gunicorn.sock           # we will communicate using this unix socket (*)
 USER=mkoenig                                       # the user to run as (*)
 GROUP=mkoenig                                      # the group to run as (*)
 NUM_WORKERS=1                                      # how many worker processes should Gunicorn spawn (*)
-DJANGO_SETTINGS_MODULE=pkdb_app.config.local       # which settings file should Django use (*)
+DJANGO_SETTINGS_MODULE=pkdb_app.config.production       # which settings file should Django use (*)
 DJANGO_WSGI_MODULE=pkdb_app.wsgi                   # WSGI module name (*)
 
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
 cd $DJANGODIR
-source /var/www/test/venv/bin/activate
+source /home/mkoenig/envs/pkdb/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
