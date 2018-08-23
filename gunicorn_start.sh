0fc65bd8337aa2c8702d2b4a7fa0cee133ecbf24
Matthias Koenig <konigmatt@googlemail.com>
2018-08-23 17:52:51 +0200

    fixing start scripts

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -23,7 +23,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec /home/mkoenig/envs/flutype_webapp/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec /home/mkoenig/envs/pkdb/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user $USER \
