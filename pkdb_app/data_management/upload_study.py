1c7f05cb402c16bf181b256833b2195b373d294f
Matthias Koenig <konigmatt@googlemail.com>
2018-08-23 19:59:05 +0200

    some work trying to post to different endpoint url

--- pkdb_app/data_management/upload_study.py
+++ pkdb_app/data_management/upload_study.py
@@ -6,7 +6,8 @@ Is used in watchdog.
 import os
 import sys
 import argparse
-import coloredlogs, logging
+import coloredlogs
+import logging
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')
@@ -18,12 +19,16 @@ from pkdb_app.data_management.fill_database import upload_study_from_dir
 
 
 def run(args):
-    upload_study_from_dir(args.study)
+    if not args.url:
+        args.url = "http://0.0.0.0:8000/api/v1"  # "http://pk-db.com/api/v1"
+
+    upload_study_from_dir(args.study, api_url=args.url)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Upload a study to PKDB")
     parser.add_argument("-s", help="directory of study", dest="study", type=str, required=True)
+    parser.add_argument("-u", help="API url to upload", dest="url", type=str, required=False)
     parser.add_argument("--r", help="create reference json", action="store_true")
 
     parser.set_defaults(func=run)
@@ -32,4 +37,5 @@ def main():
 
 
 if __name__ == "__main__":
+    #
     main()
