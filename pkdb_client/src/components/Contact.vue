1f1bca6fba4b70134c847b388f0ccd179b8ce734
Matthias Koenig <konigmatt@googlemail.com>
2018-08-25 12:28:38 +0200

    navigation menu

---
+++ pkdb_client/src/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Contact Me</h1>
+        <p>
+        <ul>
+            <li>Email: {{email}}</li>
+            <li>Website: {{web}}</li>
+        </ul>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Contact',
+        data(){
+            return {
+                email: 'konigmatt@googlemail.com',
+                web: 'https://livermetabolism.com'
+            }
+        }
+    }
+</script>
---
+++ pkdb_client/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <p>Here is some information. This is the home page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home' //this is the name of the component
+    }
+</script>
+<style>
+</style>
