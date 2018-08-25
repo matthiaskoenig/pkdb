a0b35efdf3628229128c8dc362a66e9d6b14338c
Matthias Koenig <konigmatt@googlemail.com>
2018-08-25 16:08:48 +0200

    references parsed from API

--- pkdb_client/src/components/Home.vue
+++ pkdb_client/src/components/Home.vue
@@ -1,21 +1,40 @@
 <template>
     <div>
-        <h1>Pharmacokinetics Database</h1>
+        <h1>Pharmacokinetics Database {{ statistics['version'] }}</h1>
         <ul>
-            <li>Studies: {{ studies_count }}</li>
-            <li>Groups: {{ groups_count }}</li>
-            <li>Individuals: {{ individuals_count }}</li>
-            <li>Interventions: {{ interventions_count }}</li>
-            <li>Outputs: {{ outputs_count }}</li>
-            <li>Timecourses: {{ timecourses_count }}</li>
+            <li>References: {{ statistics['reference_count'] }}</li>
+            <li>Studies: {{ statistics['study_count'] }}</li>
+            <li>Groups: {{ statistics['group_count'] }}</li>
+            <li>Individuals: {{ statistics['individual_count'] }}</li>
+            <li>Interventions: {{ statistics['intervention_count'] }}</li>
+            <li>Outputs: {{ statistics['output_count'] }}</li>
+            <li>Timecourses: {{ statistics['timecourse_count'] }}</li>
         </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Home'
+import axios from 'axios'
+export default {
+    name: 'Home',
+    data() {
+        return {
+            statistics: null,
+            errors: []
+        }
+    },
+    mounted() {
+        axios.get(`http://localhost:8000/api/v1/statistics/?format=json`)
+            .then(response => {
+                // JSON responses are automatically parsed.
+                this.statistics = response.data
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
     }
+}
 </script>
+
 <style>
 </style>
