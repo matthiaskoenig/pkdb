c6e21dd6ec6bb29c1c12e5e59dccc81ff65d8a66
Matthias Koenig <konigmatt@googlemail.com>
2018-08-25 15:07:30 +0200

    statistics endpoint for database

--- pkdb_app/statistics.py
+++ pkdb_app/statistics.py
@@ -8,22 +8,31 @@ from rest_framework.response import Response
 
 from pkdb_app._version import __version__
 from pkdb_app.studies.models import Study, Reference
+from pkdb_app.subjects.models import Group, Individual
+from pkdb_app.interventions.models import Intervention, Output, Timecourse
 
 
 class Statistics(object):
     """ Basic database statistics. """
     def __init__(self):
         self.version = __version__
-        self.studies_count = Study.objects.count()
+        self.study_count = Study.objects.count()
+        self.reference_count = Reference.objects.count()
+        self.group_count = Group.objects.count()
+        self.individual_count = Individual.objects.count()
+        self.intervention_count = Intervention.objects.count()
+        self.output_count = Output.objects.count()
+        self.timecourse_count = Timecourse.objects.count()
 
 
 class StatisticsSerializer(serializers.BaseSerializer):
     """ Serializer for database statistics. """
     def to_representation(self, instance):
-        return{
-            'version': instance.version,
-            "studies_count": instance.studies_count
-        }
+
+        return {key: getattr(instance, key) for key in ["version", "study_count",
+                                                        "reference_count", "group_count",
+                                                        "individual_count", "intervention_count", "output_count",
+                                                        "timecourse_count"]}
 
 
 class StatisticsViewSet(viewsets.ViewSet):
