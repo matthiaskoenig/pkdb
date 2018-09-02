56b993e04de294d2bd02f8daa32a84434fe9064b
Matthias Koenig <konigmatt@googlemail.com>
2018-09-02 21:32:21 +0200

    Vue templates updated

--- pkdb_client/src/components/Individuals.vue
+++ pkdb_client/src/components/Individuals.vue
@@ -1,21 +1,31 @@
 <template>
-    <div>
-        <h1>Indivduals <span v-if="count">({{ count }})</span></h1>
-        <v-paginator :resource_url="resource_url"  @update="updateResource"></v-paginator>
-        <table v-if="count" class="table table-responsive table-condensed table-hover">
-            <thead>
-            <tr>
-                <th>sid</th>
-                <th>category</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(individual, index) in entries" :key="index">
-                <td>{{ individual.sid }}</td>
-                <td>{{ individual.name }}</td>
-            </tr>
-            </tbody>
-        </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-1" />
+            <div class="col-10" >
+                <h1><font-awesome-icon icon="user" /> Indivduals <span v-if="count">({{ count }})</span></h1>
+            </div>
+            <div class="col-1" >
+                <a :href="api+'/statistics/?format=json'" title="JSON" target="_blank"><font-awesome-icon icon="code"/></a>
+            </div>
+        </div>
+        <div class="row">
+            <v-paginator :resource_url="resource_url"  @update="updateResource"></v-paginator>
+            <table v-if="count" class="table table-responsive table-condensed table-hover">
+                <thead>
+                <tr>
+                    <th>sid</th>
+                    <th>category</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(individual, index) in entries" :key="index">
+                    <td>{{ individual.sid }}</td>
+                    <td>{{ individual.name }}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
