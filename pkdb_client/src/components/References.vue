3245f288374d400d18fd3a36a86a78fd18872aa2
Matthias Koenig <konigmatt@googlemail.com>
2018-09-02 21:32:21 +0200

    Vue templates updated

--- pkdb_client/src/components/References.vue
+++ pkdb_client/src/components/References.vue
@@ -1,31 +1,41 @@
 <template>
-    <div>
-        <h1>References <span v-if="count">({{ count }})</span></h1>
-        <v-paginator :resource_url="resource_url"  @update="updateResource"></v-paginator>
-        <table v-if="count" class="table table-responsive table-condensed table-hover">
-            <thead>
-            <tr>
-                <th>sid</th>
-                <th>pmid</th>
-                <th>name</th>
-                <th>title</th>
-                <th>journal</th>
-                <th>date</th>
-                <th>abstract</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(reference, index) in references" :key="index">
-                <td>{{ reference.sid }}</td>
-                <td><a :href="'https://www.ncbi.nlm.nih.gov/pubmed/'+reference.pmid" target="_blank">{{ reference.pmid }}</a></td>
-                <td>{{ reference.name }}</td>
-                <td>{{ reference.title }}</td>
-                <td>{{ reference.journal }}</td>
-                <td>{{ reference.date }}</td>
-                <td>{{ reference.abstract }}</td>
-            </tr>
-            </tbody>
-        </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-1" />
+            <div class="col-10" >
+                <h1><font-awesome-icon icon="file-alt" /> References <span v-if="count">({{ count }})</span></h1>
+            </div>
+            <div class="col-1" >
+                <a :href="api+'/statistics/?format=json'" title="JSON" target="_blank"><font-awesome-icon icon="code"/></a>
+            </div>
+        </div>
+        <div class="row">
+            <v-paginator :resource_url="resource_url"  @update="updateResource"></v-paginator>
+            <table v-if="count" class="table table-responsive table-condensed table-hover">
+                <thead>
+                <tr>
+                    <th>sid</th>
+                    <th>pmid</th>
+                    <th>name</th>
+                    <th>title</th>
+                    <th>journal</th>
+                    <th>date</th>
+                    <th>abstract</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(reference, index) in references" :key="index">
+                    <td>{{ reference.sid }}</td>
+                    <td><a :href="'https://www.ncbi.nlm.nih.gov/pubmed/'+reference.pmid" target="_blank">{{ reference.pmid }}</a></td>
+                    <td>{{ reference.name }}</td>
+                    <td>{{ reference.title }}</td>
+                    <td>{{ reference.journal }}</td>
+                    <td>{{ reference.date }}</td>
+                    <td>{{ reference.abstract }}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
--- pkdb_client/src/components/Studies.vue
+++ pkdb_client/src/components/Studies.vue
@@ -1,49 +1,57 @@
 <template>
-    <div>
-        <h1>Studies <span v-if="count">({{ count }})</span></h1>
-
-        <v-paginator :resource_url="resource_url" @update="updateResource"></v-paginator>
-        <table v-if="count" class="table table-responsive table-condensed table-hover">
-            <thead>
-            <tr>
-                <th>sid</th>
-                <th>pkdb_version</th>
-                <th>name</th>
-                <th>reference</th>
-                <th>creator</th>
-                <th>curators</th>
-                <th>substances</th>
-                <th>design</th>
-                <th>files</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(study, index) in studies" :key="index">
-                <td>{{ study.sid }}</td>
-                <td>{{ study.pkdb_version }}</td>
-                <td>{{ study.name }}</td>
-                <td>{{ study.reference }}</td>
-                <td>{{ study.creator }}</td>
-                <td>
-                    <li v-for="(curator, index) in study.curators" :key="index">
-                        {{ curator }}
-                    </li>
-                </td>
-                <td>
-                    <li v-for="(substance, index) in study.substances" :key="index">
-                        {{ substance }}
-                    </li>
-                </td>
-                <td>{{ study.design }}</td>
-                <td>
-                    <li v-for="(file, index) in study.files" :key="index">
-                        <a :href="file">{{ file }}</a>
-                    </li>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-1" />
+            <div class="col-10" >
+                <h1><font-awesome-icon icon="file-alt" /> Studies <span v-if="count">({{ count }})</span></h1>
+            </div>
+            <div class="col-1" >
+                <a :href="api+'/statistics/?format=json'" title="JSON" target="_blank"><font-awesome-icon icon="code"/></a>
+            </div>
+        </div>
+        <div class="row">
+            <v-paginator :resource_url="resource_url" @update="updateResource"></v-paginator>
+            <table v-if="count" class="table table-responsive table-condensed table-hover">
+                <thead>
+                <tr>
+                    <th>sid</th>
+                    <th>pkdb_version</th>
+                    <th>name</th>
+                    <th>reference</th>
+                    <th>creator</th>
+                    <th>curators</th>
+                    <th>substances</th>
+                    <th>design</th>
+                    <th>files</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="(study, index) in studies" :key="index">
+                    <td>{{ study.sid }}</td>
+                    <td>{{ study.pkdb_version }}</td>
+                    <td>{{ study.name }}</td>
+                    <td>{{ study.reference }}</td>
+                    <td>{{ study.creator }}</td>
+                    <td>
+                        <li v-for="(curator, index) in study.curators" :key="index">
+                            {{ curator }}
+                        </li>
+                    </td>
+                    <td>
+                        <li v-for="(substance, index) in study.substances" :key="index">
+                            {{ substance }}
+                        </li>
+                    </td>
+                    <td>{{ study.design }}</td>
+                    <td>
+                        <li v-for="(file, index) in study.files" :key="index">
+                            <a :href="file">{{ file }}</a>
+                        </li>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
