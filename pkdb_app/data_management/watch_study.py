2c3523212318abc44b7dd1f90e033e12fb1e5da7
Matthias Koenig <konigmatt@googlemail.com>
2018-09-05 19:34:31 +0200

    direct links to file from console

--- pkdb_app/data_management/watch_study.py
+++ pkdb_app/data_management/watch_study.py
@@ -18,7 +18,7 @@ from watchdog.events import FileSystemEventHandler
 
 coloredlogs.install(
     level="INFO",
-    fmt="%(module)s:%(lineno)s %(funcName)s %(levelname) -10s %(message)s"
+    fmt="%(pathname)s:%(lineno)s %(funcName)s %(levelname) -10s %(message)s"
     # fmt="%(levelname) -10s %(asctime)s %(module)s:%(lineno)s %(funcName)s %(message)s"
 )
 logger = logging.getLogger(__name__)
