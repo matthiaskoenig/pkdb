a63647bd6b14a9f2f345542d0420befa56205456
Matthias Koenig <konigmatt@googlemail.com>
2018-09-18 19:20:08 +0200

    small changes for db upload

--- pkdb_app/data_management/fill_database.py
+++ pkdb_app/data_management/fill_database.py
@@ -49,8 +49,8 @@ from collections import namedtuple
 # master path
 # -----------------------------
 
-# API_BASE = "http://0.0.0.0:8000"
-API_BASE = "http://www.pk-db.com"
+API_BASE = "http://0.0.0.0:8000"
+# API_BASE = "http://www.pk-db.com"
 API_URL = API_BASE + "/api/v1"
 
 # -----------------------------
