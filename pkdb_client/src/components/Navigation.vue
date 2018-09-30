2744222c54e2b38135a3ca795263a4fd20096013
Matthias Koenig <konigmatt@googlemail.com>
2018-09-30 19:04:36 +0200

    bugfix admin link

--- pkdb_client/src/components/Navigation.vue
+++ pkdb_client/src/components/Navigation.vue
@@ -15,7 +15,7 @@
                 <md-tab id="tab-api" md-label="API" :md-icon="code" :href="api"></md-tab>
                 <md-tab id="tab-github" md-label="Code" :md-icon="github" href="https://www.github.com/matthiaskoenig/pkdb"></md-tab>
 
-                <md-tab id="tab-admin" md-label="Admin" :md-icon="cogs" :href="django_domain+'/admin/'"></md-tab>
+                <md-tab id="tab-admin" md-label="Admin" :md-icon="cogs" :href="domain+'/admin/'"></md-tab>
                 <md-tab id="tab-accounts" md-label="Account" :md-icon="user_circle" to="/account"></md-tab>
         </md-tabs>
 
