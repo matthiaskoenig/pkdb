b1c71c4c73754276213b16cb6456d34c177d92ed
Matthias Koenig <konigmatt@googlemail.com>
2018-10-03 14:48:55 +0200

    Fix #193, updated dependencies

--- docker_update.sh
+++ docker_update.sh
@@ -9,10 +9,10 @@ docker-compose down
 sudo ./remove_migrations.sh
 docker-compose up --build
 
-export PKDB_API_BASE="http://0.0.0.0:8000"
-export PKDB_DEFAULT_PASSWORD="pkdb"
-python pkdb_app/data_management/setup_database.py
-python pkdb_app/data_management/upload_studies.py
+# export PKDB_API_BASE="http://0.0.0.0:8000"
+# export PKDB_DEFAULT_PASSWORD="pkdb"
+# python pkdb_app/data_management/setup_database.py
+# python pkdb_app/data_management/upload_studies.py
 
 
 
