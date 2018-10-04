8c650d5d8ec199486ead22c83093a0cec65baaca
Matthias Koenig <konigmatt@googlemail.com>
2018-10-04 08:28:16 +0200

    old files

---
+++ pkdb_client/src/components/lib2/Heading.vue
@@ -0,0 +1,25 @@
+<template>
+    <md-badge :md-content="count">
+        <h1 class="md-title"><font-awesome-icon :icon="icon" /> {{ title }}</h1>
+    </md-badge>
+</template>
+
+<script>
+    export default {
+        name: "Heading",
+        props: {
+            title: {
+                type: String,
+            },
+            count: {
+                type: Number,
+            },
+            icon: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ pkdb_client/src/components/lib2/UserAvatar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="user-avatar">
+        <md-avatar class="md-avatar-icon md-small" :title="username">{{ initials }}</md-avatar>
+        <!--
+        <font-awesome-icon class="md-icon" icon="user-cog"/>
+        <font-awesome-icon icon="user-edit"/>
+        -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserAvatar",
+        props: {
+            user: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            username() {
+                return this.user.first_name + ' ' + this.user.last_name;
+            },
+            initials() {
+                return this.user.first_name[0] + this.user.last_name[0];
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
