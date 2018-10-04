8461ab64c10288f2726617983b40ae28aa8578c6
Matthias Koenig <konigmatt@googlemail.com>
2018-10-04 11:19:29 +0200

    handling icons

--- pkdb_client/src/components/Navigation.vue
+++ pkdb_client/src/components/Navigation.vue
@@ -2,21 +2,21 @@
     <div>
         <md-tabs class="md-layout">
                 <md-tab id="tab-home" md-label="Home" :md-icon="icon('home')" to="/"> </md-tab>
-                <md-tab id="tab-studies" md-label="Studies" :md-icon="procedures" to="/studies"></md-tab>
-                <md-tab id="tab-groups" md-label="Groups" :md-icon="users" to="/groups"></md-tab>
-                <md-tab id="tab-individuals" md-label="Individuals" :md-icon="user" to="/individuals"></md-tab>
-                <md-tab id="tab-interventions" md-label="Interventions" :md-icon="capsules" to="/interventions"></md-tab>
-                <md-tab id="tab-outputs" md-label="Outputs" :md-icon="chart_bar" to="/outputs"></md-tab>
-                <md-tab id="tab-timecourses" md-label="Timecourses" :md-icon="chart_line" to="/timecourses"></md-tab>
-                <md-tab id="tab-references" md-label="References"  :md-icon="file_alt" to="/references"></md-tab>
+                <md-tab id="tab-studies" md-label="Studies" :md-icon="icon('studies')" to="/studies"></md-tab>
+                <md-tab id="tab-groups" md-label="Groups" :md-icon="icon('groups')" to="/groups"></md-tab>
+                <md-tab id="tab-individuals" md-label="Individuals" :md-icon="icon('individuals')" to="/individuals"></md-tab>
+                <md-tab id="tab-interventions" md-label="Interventions" :md-icon="icon('interventions')" to="/interventions"></md-tab>
+                <md-tab id="tab-outputs" md-label="Outputs" :md-icon="icon('outputs')" to="/outputs"></md-tab>
+                <md-tab id="tab-timecourses" md-label="Timecourses" :md-icon="icon('timecourses')" to="/timecourses"></md-tab>
+                <md-tab id="tab-references" md-label="References" :md-icon="icon('references')" to="/references"></md-tab>
 
                 <!-- TODO: right aligned -->
-                <md-tab to="/about" md-active-tab="false"  id="tab-about" :md-icon="info_circle" md-label="About"></md-tab>
-                <md-tab id="tab-api" md-label="API" :md-icon="code" :href="api_url"></md-tab>
-                <md-tab id="tab-github" md-label="Code" :md-icon="github" href="https://www.github.com/matthiaskoenig/pkdb"></md-tab>
+                <md-tab to="/about" md-active-tab="false"  id="tab-about" :md-icon="icon('about')" md-label="About"></md-tab>
+                <md-tab id="tab-api" md-label="API" :md-icon="icon('api')" :href="api_url"></md-tab>
+                <md-tab id="tab-github" md-label="Code" :md-icon="icon('github')" href="https://www.github.com/matthiaskoenig/pkdb"></md-tab>
 
-                <md-tab id="tab-admin" md-label="Admin" :md-icon="cogs" :href="admin_url"></md-tab>
-                <md-tab id="tab-accounts" md-label="Account" :md-icon="user_circle" to="/account"></md-tab>
+                <md-tab id="tab-admin" md-label="Admin" :md-icon="icon('admin')" :href="admin_url"></md-tab>
+                <md-tab id="tab-accounts" md-label="Account" :md-icon="icon('account')" to="/account"></md-tab>
         </md-tabs>
 
     </div>
--- pkdb_client/src/icons.js
+++ pkdb_client/src/icons.js
@@ -10,54 +10,57 @@ function lookup_icon(key){
 
 /**
  * Lookup icon from given font-awesome branch which can be used in md-icon context.
+ * font-awesome: 5.3.1
  */
 function lookup_md_icon(key){
 
-    /*
-    if (key == 'home'){
+    if (key === 'home'){
         return require('./assets/images/fontawesome/svgs/solid/home.svg')
     }
-    */
-    /*
-    user () {
-        return require('../assets/images/fontawesome/svgs/solid/user.svg')
-    },
-    user_circle () {
-        return require('../assets/images/fontawesome/svgs/solid/user-circle.svg')
-    },
-    users () {
-        return require('../assets/images/fontawesome/svgs/solid/users.svg')
-    },
-    capsules () {
-        return require('../assets/images/fontawesome/svgs/solid/capsules.svg')
-    },
-    code () {
-        return require('../assets/images/fontawesome/svgs/solid/code.svg')
-    },
-    cogs () {
-        return require('../assets/images/fontawesome/svgs/solid/cogs.svg')
-    },
-    procedures () {
-        return require('../assets/images/fontawesome/svgs/solid/procedures.svg')
-    },
-    chart_bar () {
-        return require('../assets/images/fontawesome/svgs/solid/chart-bar.svg')
-    },
-    chart_line () {
-        return require('../assets/images/fontawesome/svgs/solid/chart-line.svg')
-    },
-    github () {
-        return require('../assets/images/fontawesome/svgs/brands/github.svg')
-    },
-    file_alt () {
-        return require('../assets/images/fontawesome/svgs/solid/file-alt.svg')
-    },
-    info_circle () {
-        return require('../assets/images/fontawesome/svgs/solid/info-circle.svg')
+    else if (['studies', 'study'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/procedures.svg')
     }
-    */
+    else if (['groups', 'group'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/users.svg')
+    }
+    else if (['individuals', 'individual'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/user.svg')
+    }
+    else if (key === 'interventions'){
+        return require('./assets/images/fontawesome/svgs/solid/capsules.svg')
+    }
+    else if (key === 'intervention'){
+        return require('./assets/images/fontawesome/svgs/solid/capsules.svg')
+    }
+    else if (['outputs', 'output'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/chart-bar.svg')
+    }
+    else if (['timecourses', 'timecourse'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/chart-line.svg')
+    }
+    else if (['references', 'reference'].indexOf(key) >= 0){
+        return require('./assets/images/fontawesome/svgs/solid/file-alt.svg')
+    }
+    else if (key === 'about'){
+        return require('./assets/images/fontawesome/svgs/solid/info-circle.svg')
+    }
+    else if (key === 'api') {
+        return require('./assets/images/fontawesome/svgs/solid/code.svg')
+    }
+    else if (key === 'github'){
+        return require('./assets/images/fontawesome/svgs/brands/github.svg')
+    }
+    else if (key === 'admin'){
+        return require('./assets/images/fontawesome/svgs/solid/cogs.svg')
+    }
+    else if (key === 'account'){
+        return require('./assets/images/fontawesome/svgs/solid/user-circle.svg')
+    }
+    // default value
+    else {
+        return require('./assets/images/fontawesome/svgs/solid/circle.svg')
+    }
 
-    return require('./assets/images/fontawesome/svgs/solid/home.svg');
 }
 
 export {lookup_md_icon};
