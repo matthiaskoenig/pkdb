a752a3ba9f583629c72bf0b6a89cc5acfaf49fdb
Matthias Koenig <konigmatt@googlemail.com>
2018-10-05 23:03:25 +0200

    About.vue fixed

---
+++ pkdb_client/src/components/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="about">
+        <v-layout row>
+            <v-flex xs12 lg6>
+                <AboutInformation/>
+            </v-flex>
+            <v-flex xs12 lg6>
+                <Statistics/>
+            </v-flex>
+        </v-layout>
+    </div>
+</template>
+
+<script>
+    import Statistics from '@/components/statistics/Statistics'
+    import AboutInformation from "./AboutInformation";
+    export default {
+        name: 'About',
+        components: {
+            AboutInformation,
+            Statistics
+        },
+    }
+</script>
