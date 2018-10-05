0614eb82a891a7f07821f2fa1e714214b127abae
Matthias Koenig <konigmatt@googlemail.com>
2018-10-05 09:50:43 +0200

    working on groups table

--- pkdb_client/src/components/detail/GroupInfo.vue
+++ pkdb_client/src/components/detail/GroupInfo.vue
@@ -32,32 +32,11 @@
                 </v-list-tile>
             </template>
         </v-list>
-
-
-        <!--
-        <md-list class="md-double-line">
-            <div class="md-list-item-text">
-                <span>{{ group.name }}</span>
-                <span>Name</span>
-            </div>
-            <div class="md-list-item-text">
-                <span>{{ group.count }}</span>
-                <span>Count</span>
-            </div>
-            <div class="md-list-item-text">
-                <span>{{ group.study_name }}</span>
-                <span>Study</span>
-            </div>
-        </md-list>
-        -->
-
-        <md-badge title="Individuals" v-if="group.individuals.length" :md-content="group.individuals.length">
-            <font-awesome-icon class="md-icon" icon="user"/>
-        </md-badge>
     </div>
 </template>
 
 <script>
+    import {lookup_icon} from "@/icons"
     export default {
         name: "GroupInfo",
         props: {
@@ -69,26 +48,32 @@
         computed: {
             items() {
                 return [
-                    { header: 'Today' },
                     {
-                        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-                        title: 'Brunch this weekend?',
-                        subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+                        subtitle: 'Name',
+                        title: this.group.name
                     },
-                    { divider: true, inset: true },
                     {
-                        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-                        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-                        subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+                        subtitle: 'Count',
+                        title: this.group.count
                     },
-                    { divider: true, inset: true },
                     {
-                        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-                        title: 'Oui oui',
-                        subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+                        subtitle: 'Study',
+                        title: this.group.study_name
+                    },
+                    {
+                        subtitle: 'Individuals',
+                        title: this.individuals_count
                     }
                 ]
+            },
+            individuals_count(){
+                return this.group.individuals.count
             }
+        },
+        methods: {
+            icon: function (key) {
+                return lookup_icon(key)
+            },
         }
     }
 </script>
