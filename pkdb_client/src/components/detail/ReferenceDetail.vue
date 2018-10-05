40702d0b20ad43919cc46b5201cc242f3aa7ca62
Matthias Koenig <konigmatt@googlemail.com>
2018-10-05 14:39:22 +0200

    Fix #195, reference view

--- pkdb_client/src/components/detail/ReferenceDetail.vue
+++ pkdb_client/src/components/detail/ReferenceDetail.vue
@@ -1,6 +1,14 @@
 <template>
     <v-card>
-        <Heading :title="'Reference: '+reference.pk" :count="1" :icon="icon('reference')" :resource_url="resource_url"/>
+        <Heading :title="'Reference: '+reference.pk" :icon="icon('reference')" :resource_url="resource_url"/>
+
+        Sid: <v-icon>{{ icon('reference') }}</v-icon>{{ reference.sid }}<br />
+        Pmid: <a :href="'https://www.ncbi.nlm.nih.gov/pubmed/'+reference.pmid" target="_blank"> {{ reference.pmid }}</a><br />
+        Name: {{ reference.name }}<br />
+        Title: {{ reference.title }}<br />
+        Journal: {{ reference.journal }}<br />
+        Date: {{reference.date}}<br />
+        Abstract: {{reference.abstract}}<br />
     </v-card>
 </template>
 
