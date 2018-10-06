26b6328c942428b521e78dad98ca5ed881f492d6
Matthias Koenig <konigmatt@googlemail.com>
2018-10-06 13:04:47 +0200

    working on button view

---
+++ pkdb_client/src/components/lib/DetailButton.vue
@@ -0,0 +1,30 @@
+<template>
+<span class="detail-button">
+    <v-btn color="primary" fab small dark :to="to" :title="title" :disabled="to ? false : true">
+        <v-icon>{{ icon }}</v-icon>
+    </v-btn>
+</span>
+</template>
+
+<script>
+    export default {
+        name: "DetailButton",
+        props: {
+            to: {
+                type: String,
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            icon: {
+                type: String,
+                required: true
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
