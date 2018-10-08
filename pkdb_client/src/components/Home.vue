915cf06fbbf143554d45849dee00b80f6f5e7447
Matthias Koenig <konigmatt@googlemail.com>
2018-10-08 15:28:18 -0400

    working on navigation

--- pkdb_client/src/components/Home.vue
+++ pkdb_client/src/components/Home.vue
@@ -7,6 +7,11 @@
             <v-flex xs6>
                 <about-information />
             </v-flex>
+
+            <v-flex xs12>
+                Navigation Test
+                <navigation-test :drawer="true"/>
+            </v-flex>
         </v-layout>
     </div>
 </template>
@@ -14,12 +19,14 @@
 <script>
     import CountTable from './statistics/CountTable'
     import AboutInformation from './AboutInformation'
+    import NavigationTest from './NavigationTest'
 
     export default {
         name: 'Home',
         components: {
             CountTable,
-            AboutInformation
+            AboutInformation,
+            NavigationTest
         }
     }
 </script>
