3691275c58f4586bc1b0e3dbe00bfcf23c40588f
Matthias Koenig <konigmatt@googlemail.com>
2018-10-19 16:26:29 +0200

    update table

--- pkdb_client/src/components/tables/InterventionsTable2.vue
+++ pkdb_client/src/components/tables/InterventionsTable2.vue
@@ -3,7 +3,11 @@
     <v-card>
         <v-card-title>
             <v-toolbar id="heading-toolbar" color="secondary" dark>
-                <Heading :count="count" :icon="icon('interventions')" title="Interventions" :Interventions="resource_url"/>
+                <heading :count="count"
+                         :icon="icon('interventions')"
+                         title="Interventions"
+                         :resource_url="url"
+                />
                 <v-spacer></v-spacer>
                 <v-text-field
                         v-model="search"
@@ -14,7 +18,6 @@
                 >
                 </v-text-field>
             </v-toolbar>
-
         </v-card-title>
 
         <v-data-table
@@ -26,28 +29,33 @@
                 class="elevation-1"
         >
             <template slot="items" slot-scope="table">
-
                 <td>
                     <LinkButton :to="'/interventions/'+ table.item.pk" :title="'Group: '+table.item.pk" :icon="icon('intervention')"/>
                     <JsonButton :resource_url="api + '/interventions_read/'+ table.item.pk +'/?format=json'"/>
                 </td>
-                <td> <text-highlight :queries="[search]"> {{table.item.name }} </text-highlight></td>
-                <td>  <text-highlight :queries="[search]"> {{table.item.category }}</text-highlight></td>
-                <td>  <text-highlight :queries="[search]"> {{table.item.choice }} </text-highlight></td>
                 <td>
-                    <text-highlight :queries="[search]"> {{table.item.application }} </text-highlight><br/>
-                    <text-highlight :queries="[search]">{{table.item.time}} </text-highlight> <span v-if="table.item.time_unit">[ <text-highlight :queries="[search]">{{table.item.time_unit }} </text-highlight> ]</span><br />
-                    <text-highlight :queries="[search]">{{ table.item.route }} </text-highlight> <br/>
-                    <text-highlight :queries="[search]">{{table.item.form}} </text-highlight>
+                    <text-highlight :queries="[search]">{{table.item.name }}</text-highlight>
                 </td>
                 <td>
-                    <substance-chip :substance="table.item.substance.name" :search="search"/>
+                    <text-highlight :queries="[search]"> {{table.item.category }}</text-highlight>
+                </td>
+                <td>
+                    <text-highlight :queries="[search]"> {{table.item.choice }}</text-highlight>
+                </td>
+                <td>
+                    <text-highlight :queries="[search]"> {{table.item.application }}</text-highlight><br/>
+                    <text-highlight :queries="[search]">{{table.item.time}}</text-highlight> <span v-if="table.item.time_unit"> [<text-highlight :queries="[search]">{{table.item.time_unit }}</text-highlight>]</span><br />
+                    <text-highlight :queries="[search]">{{ table.item.route }}</text-highlight> <br/>
+                    <text-highlight :queries="[search]">{{table.item.form}}</text-highlight>
+                </td>
+                <td>
+                    <substance-chip v-if="table.item.substance" :title="table.item.substance.name" :search="search"/>
                 </td>
                 <td><characteristica-card :data="table.item" /></td>
             </template>
 
             <template slot="no-data">
-                <v-alert :value="true" color="error" icon="fas fa-exclamation">
+                <v-alert :value="true" color="warning" icon="fas fa-exclamation">
                     Sorry, nothing to display here :(
                 </v-alert>
             </template>
@@ -113,44 +121,38 @@
             resource_url() {
                 return this.$store.state.endpoints.api  + '/interventions_elastic/?format=json'
             },
+            url() {
+                let url = this.resource_url
+                    + '&page='+ this.pagination.page
+                    + '&page_size='+ this.pagination.rowsPerPage
+                    + '&ordering='+ this.descending+ this.pagination.sortBy;
+                if(this.search){
+                    url += '&search='+ this.search
+                }
+                return url
+            },
             descending() {
-                if(this.pagination.descending){
-                    return "-";
-                }
-                else{
-                    return ""
-                }
+                return (this.pagination.descending ? "-" : "");
             }
         },
 
         methods: {
-            icon: function (key) {
+            icon(key) {
                 return lookup_icon(key)
             },
+
             getData() {
-
-                let url = this.$store.state.endpoints.api
-                    + '/interventions_elastic/?format=json'
-                    +'&page='+ this.pagination.page
-                    +'&page_size='+ this.pagination.rowsPerPage
-                    +'&ordering='+ this.descending+ this.pagination.sortBy;
-                if(this.search){
-                    url += '&search='+ this.search
-                }
-
-                axios.get(url)
+                axios.get(this.url)
                     .then(res => {
                         this.entries = res.data.data.data;
                         this.count = res.data.data.count;
                     })
                     .catch(err => console.log(err.response.data))
                     .finally(() => this.loading = false);
-
             }
         }
     }
 </script>
 
 <style scoped>
-
 </style>
