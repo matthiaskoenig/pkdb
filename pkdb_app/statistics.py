bfeb0f67117f0d6577f2b2008df1e7e9134f96a1
Jan <janekg89@hotmail.de>
2019-01-21 18:36:06 +0100

    first version of statistics

--- pkdb_app/statistics.py
+++ pkdb_app/statistics.py
@@ -22,9 +22,9 @@ class Statistics(object):
         self.reference_count = Reference.objects.count()
         self.group_count = Group.objects.count()
         self.individual_count = Individual.objects.count()
-        self.intervention_count = Intervention.objects.count()
-        self.output_count = Output.objects.count()
-        self.timecourse_count = Timecourse.objects.count()
+        self.intervention_count = Intervention.objects.filter(final=True).count()
+        self.output_count = Output.objects.filter(final=True).count()
+        self.timecourse_count = Timecourse.objects.filter(final=True).count()
 
 
 class StatisticsViewSet(viewsets.ViewSet):
