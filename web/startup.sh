bdb26faf154050ca59550b805a973121afd2043c
Matthias Koenig <konigmatt@googlemail.com>
2019-01-25 16:33:08 +0100

    working on startup script

---
+++ web/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Migrates the database, uploads staticfiles, and runs the production server
+
+
+https://docs.docker.com/compose/startup-order/
+# FIXME: requires database
+# python ./manage.py makemigrations
+# python ./manage.py migrate
+# python ./manage.py createsuperuser2 --username admin --password $DEFAULT_PASSWORD --email koenigmx@hu-berlin.de --noinput
+
+python ./manage.py collectstatic --noinput
+
+
+/usr/local/bin/gunicorn docker_django.wsgi:application -w 2 -b :8000
