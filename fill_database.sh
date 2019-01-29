8df4a2349de81db8fb5bf18903a20ab5752c1721
Matthias Koenig <konigmatt@googlemail.com>
2019-01-29 13:01:14 +0100

    upload scripts

--- fill_database.sh
+++ fill_database.sh
@@ -3,5 +3,5 @@
 # Setup database and uploads studies
 #   set -a && source .env.local && ./fill_database.sh
 ########################################################
-
+python backend/pkdb_app/data_management/setup_database.py
 python backend/pkdb_app/data_management/upload_studies.py
