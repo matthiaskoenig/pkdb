01013bc5bb0065ee238deb03f4404adb5efa6f63
Matthias Koenig <konigmatt@googlemail.com>
2019-02-04 15:35:50 +0100

    working on docker update script

---
+++ docker_update.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+########################################################
+# Complete update
+########################################################
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# shut down all containers (remove images and volumes)
+# remove images and volumes
+docker-compose down --volumes --rmi local
+
+# make sure containers are removed (if not running)
+docker container rm -f pkdb_setup_root_1 pkdb_migration_1 pkdb_frontend_1 pkdb_backend_1 pkdb_postgres_1 pkdb_elasticsearch_1 
+
+# make sure images are removed
+docker image rm -f pkdb_frontend:latest
+docker image rm -f pkdb_backend:latest
+docker image rm -f pkdb_migration:latest
+docker image rm -f pkdb_setup_root:latest
+
+# make sure volumes are removed
+docker volumes rm -f pkdb_django_media
+docker volumes rm -f pkdb_django_static
+docker volumes rm -f pkdb_elasticsearch_data
+docker volumes rm -f pkdb_node_modules
+docker volumes rm -f pkdb_postgres_data
+docker volumes rm -f pkdb_vue_dist
+
+# cleanup all dangling images, containers, volumes and networks
+docker system prune --force
+
+# remove migrations
+cd $DIR
+sudo find . -maxdepth 3 -path "*/migrations/*.py" -not -name "__init__.py" -delete
+sudo find . -maxdepth 3 -path "*/migrations/*.pyc" -delete
+
+# remove media and static files
+sudo rm -rf media
+sudo rm -rf static
+
+# start containers
+set -a && source .env.local && docker-compose up --build 
+
+# elasticsearch index
+docker-compose run --rm backend ./manage.py search_index --create -f
