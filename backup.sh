9b0bae0398217cb4e181a337ab2da5992ff93eea
Matthias Koenig <konigmatt@googlemail.com>
2019-05-02 17:26:15 +0200

    backup script added

--- backup.sh
+++ backup.sh
@@ -12,30 +12,43 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 echo "-------------------------------"
-echo "Backup PKDB docker volumes"
+echo "Create backup directory"
 echo "-------------------------------"
-cd /var/git/pkdb
 
 # create directory for backup
 BACKUP_DIR=/var/backups/pkdb/$(date '+%F')
 sudo mkdir -p $BACKUP_DIR
 sudo chown -R mkoenig:mkoenig $BACKUP_DIR
 echo "Backup to" $BACKUP_DIR
-
-
-echo *** backup django volumes***
 cd $BACKUP_DIR
+
+echo "-------------------------------"
+echo "Backup PKDB docker volumes"
+echo "-------------------------------"
+
+echo "*** django_media ***"
 docker run --rm --volumes-from pkdb_backend_1 -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/django_media.tar.gz /media
+
+echo "*** django_static ***"
 docker run --rm --volumes-from pkdb_backend_1 -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/django_static.tar.gz /static
+
+echo "*** postgres_data ***"
 docker run --rm --volumes-from pkdb_postgres_1 -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/postgres_data.tar.gz /var/lib/postgresql/data
+
+echo "*** elasticsearch_data ***"
 docker run --rm --volumes-from pkdb_elasticsearch_1 -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/elasticsearch_data.tar.gz /usr/share/elasticsearch/data
+
+echo "*** vue_dist ***"
 docker run --rm --volumes-from pkdb_frontend_1 -v $BACKUP_DIR:/backup ubuntu tar cvzf /backup/vue_dist.tar.gz /vue
 
 
 echo "----------------------------"
 echo "Dump PKDB database"
 echo "----------------------------"
-# sudo -u postgres pg_dump pkdb > $DIR/pkdb_dump.psql
-docker exec -u $PKDB_DB_USER pkdb_postgres1 pg_dump -Fc $PKDB_DB_NAME > db.dump
-
-cd $DIR
+docker exec -u $PKDB_DB_USER pkdb_postgres_1 pg_dump -Fc $PKDB_DB_NAME > pkdb.dump
+if [ -e $BACKUP_DIR/pkdb.dump ]
+then
+    echo "SUCCESS $BACKUP_DIR/pkdb.dump"
+else
+    echo "! FAILURE $BACKUP_DIR/pkdb.dump !"
+fi
