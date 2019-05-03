b275e4700d015727eb63c3935b24e35b35317570
Jan <janekg89@hotmail.de>
2019-05-03 14:46:52 +0200

    started on test

--- backend/pkdb_app/tests/statistics.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == "__main__":
-    import requests
-
-    API_URL = "http://0.0.0.0:8000/api/v1"
-    response = requests.get(f"{API_URL}/statistics/")
-    print(response.status_code)
-    print(response.text)
