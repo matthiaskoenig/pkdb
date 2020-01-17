1cadbf284b77b49a8d003947f601d3b367551c94
Matthias Koenig <konigmatt@googlemail.com>
2020-01-17 23:28:01 +0100

    fix buttons in navigation menu

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,14 +1,14 @@
 <template>
     <v-toolbar id="navigation" class="fixed-nav-bar" flat dense>
 
-        <v-btn icon="blue" to="/" title="Home"><v-icon color="black">{{ icon('home') }}</v-icon></v-btn>
-        <v-btn icon to="/studies" title="Studies"><v-icon color="black">{{ icon('studies') }}</v-icon></v-btn>
-        <v-btn icon to="/groups" title="Groups"><v-icon color="black">{{ icon('groups') }}</v-icon></v-btn>
-        <v-btn icon to="/individuals" title="Individuals" color="black"><v-icon>{{ icon('individuals') }}</v-icon></v-btn>
-        <v-btn icon to="/interventions" title="Interventions" color="black"><v-icon>{{ icon('interventions') }}</v-icon></v-btn>
-        <v-btn icon to="/outputs" title="Outputs"><v-icon color="black">{{ icon('outputs') }}</v-icon></v-btn>
-        <v-btn icon to="/timecourses" title="Timecourses"><v-icon color="black">{{ icon('timecourses') }}</v-icon></v-btn>
-        <v-btn icon to="/references" title="References"><v-icon color="black">{{ icon('references') }}</v-icon></v-btn>
+        <v-btn icon to="/" title="Home" color="white"><v-icon>{{ icon('home') }}</v-icon></v-btn>
+        <v-btn icon to="/studies" title="Studies" color="white"><v-icon>{{ icon('studies') }}</v-icon></v-btn>
+        <v-btn icon to="/groups" title="Groups" color="white"><v-icon>{{ icon('groups') }}</v-icon></v-btn>
+        <v-btn icon to="/individuals" title="Individuals" color="white"><v-icon>{{ icon('individuals') }}</v-icon></v-btn>
+        <v-btn icon to="/interventions" title="Interventions" color="white"><v-icon>{{ icon('interventions') }}</v-icon></v-btn>
+        <v-btn icon to="/outputs" title="Outputs" color="white"><v-icon>{{ icon('outputs') }}</v-icon></v-btn>
+        <v-btn icon to="/timecourses" title="Timecourses" color="white"><v-icon>{{ icon('timecourses') }}</v-icon></v-btn>
+        <v-btn icon to="/references" title="References" color="white"><v-icon>{{ icon('references') }}</v-icon></v-btn>
 
         <v-spacer></v-spacer>
 
@@ -18,18 +18,18 @@
             {{ username }}
         </v-chip>
         <v-btn icon v-if="username==null" title="Login" @click.stop="dialog=true">
-            <v-icon color="black">{{ icon('account') }}</v-icon>
+            <v-icon color="white">{{ icon('account') }}</v-icon>
         </v-btn>
         <v-dialog v-model="dialog" max-width="500">
             <user-login></user-login>
         </v-dialog>
 
         <!-- links -->
-        <v-btn icon to="/curation" title="Curation information"><v-icon color="black">{{ icon('curation') }}</v-icon></v-btn>
-        <v-btn icon :href="api_url" title="REST API"><v-icon color="black">{{ icon('api') }}</v-icon></v-btn>
-        <v-btn icon :href="admin_url" title="Django admin interface"><v-icon color="black">{{ icon('admin') }}</v-icon></v-btn>
-        <v-btn icon href="https://www.github.com/matthiaskoenig/pkdb" title="GitHub code repository">
-            <v-icon color="black">{{ icon('github')}}</v-icon>
+        <v-btn icon to="/curation" title="Curation information" color="white"><v-icon>{{ icon('curation') }}</v-icon></v-btn>
+        <v-btn icon :href="api_url" title="REST API" color="white"><v-icon>{{ icon('api') }}</v-icon></v-btn>
+        <v-btn icon :href="admin_url" title="Django admin interface" color="white"><v-icon>{{ icon('admin') }}</v-icon></v-btn>
+        <v-btn icon href="https://www.github.com/matthiaskoenig/pkdb" title="GitHub code repository" color="white">
+            <v-icon>{{ icon('github')}}</v-icon>
         </v-btn>
     </v-toolbar>
 </template>
@@ -75,6 +75,6 @@
         z-index: 9999;
         width: 100%;
         height: 50px;
-        background-color: #00a087;
+        background-color: #000000;
     }
 </style>
