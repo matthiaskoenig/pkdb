6fb575011a2f8ade6848f495b91e59107aa7457a
Matthias Koenig <konigmatt@googlemail.com>
2020-01-18 20:31:19 +0100

    headings in study view fixed

--- frontend/src/components/detail/StudyDetail.vue
+++ frontend/src/components/detail/StudyDetail.vue
@@ -45,29 +45,35 @@
 
                 <!-- Groups -->
                 <v-flex ref="groups" xs12 v-show="visible.groups">
+                    <count-chip :count="study.group_count" :icon="icon('group')" name="group"></count-chip>
                     <annotations  v-if="study.groupset" :item="study.groupset"/>
                     <groups-table  v-if="study.group_count>0" :ids="study.groupset.groups" :autofocus="false"/>
                 </v-flex>
 
                 <!-- Individuals -->
                 <v-flex  ref="individuals" xs12 v-show="visible.individuals">
+                    <count-chip :count="study.individual_count" :icon="icon('individual')" name="individual"></count-chip>
                     <annotations   v-if="study.individualset" :item="study.individualset"/>
                     <individuals-table v-if="study.individual_count>0" :ids="study.individualset.individuals" :autofocus="false"/>
                 </v-flex>
 
                 <!-- Interventions -->
                 <v-flex  ref="interventions" xs12 v-show="visible.interventions">
+                    <count-chip :count="study.intervention_count" :icon="icon('intervention')" name="intervention"></count-chip>
                     <annotations v-if="study.interventionset" :item="study.interventionset"/>
                     <interventions-table v-if="study.intervention_count>0" :ids="study.interventionset.interventions" :autofocus="false"/>
                 </v-flex>
 
                 <!-- Outputs -->
                 <v-flex xs12 v-show="visible.outputs || visible.timecourses">
+
                     <annotations  v-if="study.outputset" :item="study.outputset"/>
                     <span ref="outputs"></span>
+                    <count-chip :count="study.output_count" :icon="icon('output')" name="output"></count-chip>
                     <outputs-table v-if="study.output_count>0" v-show="visible.outputs" :ids="study.outputset.outputs" :autofocus="false"/>
                     <br />
                     <span ref="timecourses"></span>
+                    <count-chip :count="study.timecourse_count" :icon="icon('timecourse')" name="timecourse"></count-chip>
                     <timecourses-table v-if="study.timecourse_count>0" v-show="visible.timecourses" :ids="study.outputset.timecourses" :autofocus="false"/>
                 </v-flex>
             </v-layout>
