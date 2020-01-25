81a4ba80e81f6bbdbbd72043a6ebfa8082c983c4
Jan Grzegorzewski <janekg89@hotmail.de>
2020-01-25 21:34:36 +0100

    debuging

--- backend/pkdb_app/subjects/serializers.py
+++ backend/pkdb_app/subjects/serializers.py
@@ -98,7 +98,8 @@ class CharacteristicaSerializer(MeasurementTypeableSerializer):
         try:
             # perform via dedicated function on categorials
             attrs['measurement_type'] = attrs['measurement_type'].measurement_type
-            if "substance" in attrs:
+            if "substance" in attrs and  attrs["substance"] is not None:
+
                 attrs['substance'] = attrs['substance'].substance
 
             attrs['measurement_type'].validate_complete(data=attrs)
