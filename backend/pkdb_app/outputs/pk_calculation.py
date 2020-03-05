f4ae583131f5d37a1c0ab812a7e92f33d410663b
Matthias Koenig <konigmatt@googlemail.com>
2020-03-05 23:32:27 +0100

    updating pharmacokinetics calculation

--- backend/pkdb_app/outputs/pk_calculation.py
+++ backend/pkdb_app/outputs/pk_calculation.py
@@ -22,14 +22,13 @@ def pkoutputs_from_timecourse(tc: Timecourse) -> List[Dict]:
     outputs = []
     tc_type = tc.measurement_type.info_node.name
 
+    # pharmacokinetics are only calculated on normalized concentrations
     if tc_type == "concentration" and tc.normed:
-        # pharmacokinetics are only calculated on normalized concentrations
-
         variables = timecourse_to_pkdict(tc)
         c_type = variables.pop("c_type", None)
-        _ = variables.pop("bodyweight_type", None)
 
-        pk = pharmacokinetics.f_pk(**variables)
+        pkinf = pharmacokinetics.PKInference(**variables)
+        pk = pkinf.pk
 
         key_mapping = {
             "auc": MeasurementType.objects.get(info_node__name="auc_end"),
@@ -40,24 +39,26 @@ def pkoutputs_from_timecourse(tc: Timecourse) -> List[Dict]:
             "thalf": MeasurementType.objects.get(info_node__name="thalf"),
             "tmax": MeasurementType.objects.get(info_node__name="tmax"),
             "vd": MeasurementType.objects.get(info_node__name="vd"),
+            "vdss": MeasurementType.objects.get(info_node__name="vdss"),
         }
 
-        for key in ["auc", "aucinf", "cl", "cmax", "kel", "thalf", "vd", "tmax"]:
-            pk_unit = pk[f"{key}_unit"]
+        for key in key_mapping.keys():
+            pk_par = getattr(pk, key)
             if not np.isnan(pk[key]):
                 output_dict = {}
-                output_dict[c_type] = pk[key]
-                output_dict["unit"] = pk_unit
+                output_dict[c_type] = pk_par.magnitude
+                output_dict["unit"] = pk_par.units
                 output_dict["measurement_type"] = key_mapping[key]
                 output_dict["calculated"] = True
                 output_dict["tissue"] = tc.tissue
                 output_dict["substance"] = tc.substance
                 output_dict["group"] = tc.group
                 output_dict["individual"] = tc.individual
+                output_dict["study"] = tc.study
                 if output_dict["measurement_type"].info_node.name == "auc_end":
                     output_dict["time"] = max(tc.time)
                     output_dict["time_unit"] = tc.time_unit
-                output_dict["study"] = tc.study
+
                 outputs.append(output_dict)
 
     return outputs
@@ -69,29 +70,23 @@ def timecourse_to_pkdict(tc: Timecourse) -> Dict:
     :return: dict
     """
     pk_dict = {}
-
+    
     # substance
-    pk_dict["compound"] = tc.substance.info_node.name
-
-    bodyweight = tc.get_bodyweight().first()
+    pk_dict["substance"] = tc.substance.info_node.name
 
     # time
     pk_dict["t"] = pd.Series(tc.time)
     pk_dict["t_unit"] = tc.time_unit
 
     # concentration
-    # FIXME: the timecourse data must be filtered based on the dosing times
-    #   (or alternatively this should be handled in the pk calculation)
     pk_dict["c_unit"] = tc.unit
 
     if tc.mean:
         pk_dict["c"] = pd.Series(tc.mean)
         pk_dict["c_type"] = "mean"
-
     elif tc.median:
         pk_dict["c"] = pd.Series(tc.median)
         pk_dict["c_type"] = "median"
-
     elif tc.value:
         pk_dict["c"] = pd.Series(tc.value)
         pk_dict["c_type"] = "value"
@@ -112,21 +107,5 @@ def timecourse_to_pkdict(tc: Timecourse) -> Dict:
                         tc.time_unit).magnitude
 
                 pk_dict["dose_unit"] = dosing.unit
-
-    # bodyweight dependent values
-    if bodyweight:
-        pk_dict["bodyweight_unit"] = bodyweight.unit
-
-        if bodyweight.value:
-            pk_dict["bodyweight"] = bodyweight.value
-            pk_dict["bodyweight_type"] = "value"
-
-        elif bodyweight.mean:
-            pk_dict["bodyweight"] = bodyweight.mean
-            pk_dict["bodyweight_type"] = "mean"
-
-        elif bodyweight.median:
-            pk_dict["bodyweight"] = bodyweight.median
-            pk_dict["bodyweight_type"] = "median"
 
     return pk_dict
