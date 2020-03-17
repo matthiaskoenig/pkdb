Fix #532, bugfix cv calculation and division by zero issues

backend/pkdb_app/error_measures.py
@@ -1,9 +1,16 @@
-# sd is standard deviation
-# se is standard error (standard deviation of the mean) sd/sqrt(n)
-# cv is coeficient of variance. sd/mean
-import numpy as np
+"""
+Helper functions for calculation of error measures from other errors.
 
-def get_sd(se, count, cv, mean):
+    sd is standard deviation
+    se is standard error (standard deviation of the mean) sd/sqrt(n)
+    cv is coefficient of variation. sd/mean
+"""
+import numpy as np
+import warnings
+
+
+def calculate_sd(se, count, cv, mean):
+    """Calculates standard deviation from other error measurements."""
     sd = None
     is_se = sd is not None
     is_count = count is not None
@@ -24,7 +31,8 @@ def get_sd(se, count, cv, mean):
     return sd
 
 
-def get_se(sd, count, cv, mean):
+def calculate_se(sd, count, cv, mean):
+    """Calculates SE from given fields."""
     if isinstance(sd, list):
         sd = list(map(float, sd))
     if isinstance(mean, list):
@@ -39,14 +47,14 @@ def get_se(sd, count, cv, mean):
     is_cv = cv is not None
 
     if is_sd and is_count:
-
         se = np.true_divide(sd, np.sqrt(count))
     elif is_count and is_mean and is_cv:
         se = np.true_divide((np.multiply(cv, mean)), np.sqrt(count))
     return se
 
 
-def get_cv(sd, count, se, mean):
+def calculate_cv(sd, count, se, mean):
+    """Calculates CV from given fields"""
     if isinstance(sd, list):
         sd = list(map(float, sd))
     if isinstance(mean, list):
@@ -60,8 +68,14 @@ def get_cv(sd, count, se, mean):
     is_mean = mean is not None
     is_se = se is not None
 
+    # mean can be zero, CV not calculatable, resulting in -inf/inf
+    # mean data must be cleaned before calculation
+    mean_clean = np.copy(mean)
+    mean_clean[mean_clean == 0.0] = np.nan
+
     if is_sd and is_mean:
-        cv = np.true_divide(sd, mean)
+        cv = np.true_divide(sd, mean_clean)
     elif is_se and is_count and is_mean:
-        cv = np.true_divide(np.multiply(se, np.sqrt(count)), mean)
+        cv = np.true_divide(np.multiply(se, np.sqrt(count)), mean_clean)
+
     return cv

backend/pkdb_app/outputs/models.py
@@ -20,7 +20,7 @@ from .managers import (
 )
 from ..behaviours import (
     Externable, Accessible)
-from ..normalization import get_cv, get_se, get_sd
+from ..error_measures import calculate_cv, calculate_se, calculate_sd
 from ..utils import CHAR_MAX_LENGTH, CHAR_MAX_LENGTH_LONG
 
 TIME_NORM_UNIT = "hr"
@@ -196,15 +196,15 @@ class Output(AbstractOutput, Outputable, Accessible):
     def add_error_measures(self):
         if self.group:
             if not self.sd:
-                self.sd = get_sd(
+                self.sd = calculate_sd(
                     se=self.se, count=self.group.count, mean=self.mean, cv=self.cv
                 )
             if not self.se:
-                self.se = get_se(
+                self.se = calculate_se(
                     sd=self.sd, count=self.group.count, mean=self.mean, cv=self.cv
                 )
             if not self.cv:
-                self.cv = get_cv(
+                self.cv = calculate_cv(
                     se=self.se, count=self.group.count, mean=self.mean, sd=self.sd
                 )
 
@@ -299,19 +299,19 @@ class Timecourse(AbstractOutput, Outputable, Accessible):
         """ Calculates additional error measures."""
         if self.group:
             if not self.sd:
-                sd = get_sd(
+                sd = calculate_sd(
                     se=self.se, count=self.group.count, mean=self.mean, cv=self.cv
                 )
                 if isinstance(sd, np.ndarray):
                     self.sd = list(sd)
             if not self.se:
-                se = get_se(
+                se = calculate_se(
                     sd=self.sd, count=self.group.count, mean=self.mean, cv=self.cv
                 )
                 if isinstance(se, np.ndarray):
                     self.se = list(se)
             if not self.cv:
-                cv = get_cv(
+                cv = calculate_cv(
                     se=self.se, count=self.group.count, mean=self.mean, sd=self.sd
                 )
                 if isinstance(cv, np.ndarray):