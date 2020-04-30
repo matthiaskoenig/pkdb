ed1987c728648d642648f41db16116f7ab15c26e
Matthias Koenig <konigmatt@googlemail.com>
2020-05-01 01:38:44 +0200

    fix for virtualenv activation

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 # -----------------------------------------------------------------------------
 # Automatic deployment develop.pk-db.com
 # -----------------------------------------------------------------------------
+# sudo ./deploy.sh 2>&1 | tee ./deploy.logsudo ./deploy.sh 2>&1 | tee ./deploy.log
 # - !necessary to have cached credentials for pulling updates
 # FIXME: run in cronjob (sudo cron)
 # FIXME: logging of all output for debugging
 # FIXME: logging of study uploads and errors for dayly report
 #
 # -----------------------------------------------------------------------------
+
+echo "-------------------------"
+echo "UPDATE PKDB"
+echo "-------------------------"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
+echo $DIR
 # pull latest changes
 git pull
 # set environment variables
@@ -19,8 +25,15 @@ set -a && source .env.production
 # create info nodes & upload study (from server locally)
 cd ../pkdb_data
 git pull
+
+# FIXME: how to set environment variables in shell
 set -a && source .env.develop
-workon pkdb_data
+
+echo "-------------------------"
+echo "UPLOAD DATA"
+echo "-------------------------"
+# activate environment on server
+source $HOME/envs/pkdb_data/bin/activate
 pip install -e . --upgrade
 upload_nodes
 upload_studies
