b053bbbd57d722d61ab6ac86ac64921ad4b5da9a
Matthias Koenig <konigmatt@googlemail.com>
2020-05-06 17:00:12 +0200

    fix backup sudo

--- backup.sh
+++ backup.sh
@@ -19,8 +19,8 @@ echo "-------------------------------"
 
 # create directory for backup
 BACKUP_DIR=/var/backups/pkdb/$(date '+%F')
-# sudo mkdir -p $BACKUP_DIR
-# sudo chown -R mkoenig:mkoenig $BACKUP_DIR
+mkdir -p $BACKUP_DIR
+
 echo "Backup to" $BACKUP_DIR
 cd $BACKUP_DIR
 
