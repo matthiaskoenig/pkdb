9b72ad79454f26b0dc7f0f92faaeb35fd8bd354c
Matthias Koenig <konigmatt@googlemail.com>
2020-05-06 16:55:36 +0200

    removed sudo from purge command

--- docker-purge.sh
+++ docker-purge.sh
@@ -8,16 +8,17 @@
 #     ./docker-purge.sh
 # -----------------------------------------------------------------------------
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-: "${PKDB_DOCKER_COMPOSE_YAML:?The PKDB environment variable must be exported: set -a && source .env.local}"
+: "${PKDB_DOCKER_COMPOSE_YAML:?The PKDB environment variable must be exported: set -a && source .env.*}"
 
-sudo echo "Purging database and all docker containers, volumes, images ($PKDB_DOCKER_COMPOSE_YAML)?"
-#FIXME: add argument to skip question
-#read -p "Are you sure [y/N]? " -n 1 -r
-#echo    # (optional) move to a new line
-#if [[ ! $REPLY =~ ^[Yy]$ ]]
-#then
-#    exit 1
-#fi
+# remove migrations & static media (probably necessary to run as sudo)
+cd $DIR
+find . -maxdepth 5 -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -maxdepth 5 -path "*/migrations/*.pyc" -delete
+
+# remove media and static files
+rm -rf media
+rm -rf static
+
 
 # shut down all containers (remove images and volumes)
 docker-compose -f $PKDB_DOCKER_COMPOSE_YAML down --volumes --rmi local
@@ -46,15 +47,6 @@ docker volume rm -f pkdb_vue_dist
 
 # cleanup all dangling images, containers, volumes and networks
 docker system prune --force
-
-# remove migrations
-cd $DIR
-sudo find . -maxdepth 5 -path "*/migrations/*.py" -not -name "__init__.py" -delete
-sudo find . -maxdepth 5 -path "*/migrations/*.pyc" -delete
-
-# remove media and static files
-sudo rm -rf media
-sudo rm -rf static
 
 # build and start containers
 docker-compose -f $PKDB_DOCKER_COMPOSE_YAML build --no-cache
