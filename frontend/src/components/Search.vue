588af4b492e8e6d44bd582a6f0bb8893704cc974
Matthias Koenig <konigmatt@googlemail.com>
2020-08-09 13:12:28 +0200

    minor fixes

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,111 +1,108 @@
 <template>
-    <div id="Search">
-          <v-layout row wrap>
-            <v-flex xs6>
-              <study-form
-                  @studies__name__in="update_search_query"
-                  @studies__reference_name__in="update_search_query"
-              />
-            </v-flex>
-            <v-flex xs6>
-              <subjects-form/>
-            </v-flex>
-            <v-flex xs6>
-              <intervention-form
-                  @interventions__substance_name__in="update_search_query"
-                  @interventions__route_name__in="update_search_query"
-                  @interventions__application_name__in="update_search_query"
-                  @interventions__form_name_in="update_search_query"
+  <div id="Search">
+    <v-layout row wrap>
+      <v-flex xs6>
+        <study-form
+            @studies__name__in="update_search_query"
+            @studies__reference_name__in="update_search_query"
+        />
+      </v-flex>
+      <v-flex xs6>
+        <subjects-form/>
+      </v-flex>
+      <v-flex xs6>
+        <intervention-form
+            @interventions__substance_name__in="update_search_query"
+            @interventions__route_name__in="update_search_query"
+            @interventions__application_name__in="update_search_query"
+            @interventions__form_name_in="update_search_query"
+        />
+      </v-flex>
+      <v-flex xs6>
+        <output-form/>
+      </v-flex>
 
-              />
-            </v-flex>
-            <v-flex xs6>
-              <output-form/>
-            </v-flex>
-          <v-flex xs2>studies:{{study_count}}</v-flex>
-          <v-flex xs2>interventions: {{intervention_count}}</v-flex>
-          <v-flex xs2>groups: {{group_count}}</v-flex>
-          <v-flex xs2>individuals: {{individual_count}}</v-flex>
-          <v-flex xs2>outputs: {{output_count}}</v-flex>
-          <v-flex xs2>
-            <v-progress-circular
-              indeterminate
-              color="primary"
-              v-if="loading"
-          >
+      <v-flex xs2>studies: {{ study_count }}</v-flex>
+      <v-flex xs2>interventions: {{ intervention_count }}</v-flex>
+      <v-flex xs2>groups: {{ group_count }}</v-flex>
+      <v-flex xs2>individuals: {{ individual_count }}</v-flex>
+      <v-flex xs2>outputs: {{ output_count }}</v-flex>
+      <v-flex xs2>
 
-            </v-progress-circular>
-          </v-flex>
+        <v-progress-circular
+            indeterminate
+            color="primary"
+            v-if="loading"
+        >
+        </v-progress-circular>
+      </v-flex>
 
-          </v-layout>
-
-
-    </div>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-  import StudyForm from "./forms/StudyForm";
-  import InterventionForm from "./forms/InterventionForm";
-  import SubjectsForm from "./forms/SubjectsForm";
-  import OutputForm from "./forms/OutputForm";
-  import {searchTableMixin} from "./tables/mixins";
+import StudyForm from "./forms/StudyForm";
+import InterventionForm from "./forms/InterventionForm";
+import SubjectsForm from "./forms/SubjectsForm";
+import OutputForm from "./forms/OutputForm";
+import {searchTableMixin} from "./tables/mixins";
 
-  import axios from 'axios'
+import axios from 'axios'
 
-  export default {
-      mixins: [searchTableMixin],
+export default {
+  mixins: [searchTableMixin],
 
-      name: 'Search',
-        components: {
-          StudyForm,
-          InterventionForm,
-          SubjectsForm,
-          OutputForm
-        },
-      computed: {
-        url() {
-          var url = this.resource_url
+  name: 'Search',
+  components: {
+    StudyForm,
+    InterventionForm,
+    SubjectsForm,
+    OutputForm
+  },
+  computed: {
+    url() {
+      let url = this.resource_url
 
-          for ( const [key, value]  of Object.entries(this.queries)){
-            if (value.length > 0){
-              url = url + "&"+ key +"=" +value.join("__")
-            }
-          }
-          return url
-        },
-      },
-      methods: {
-        update_search_query(emitted_object){
-          for (const [key, value] of Object.entries(emitted_object)){
+      for (const [key, value] of Object.entries(this.queries)) {
+        if (value.length > 0) {
+          url = url + "&" + key + "=" + value.join("__")
+        }
+      }
+      return url
+    },
+  },
+  methods: {
+    update_search_query(emitted_object) {
+      for (const [key, value] of Object.entries(emitted_object)) {
 
-            this.queries[key] = value
-          }
-        },
-        getData() {
-          this.loading = true
-          if (localStorage.getItem('token')) {
-            var headers = {Authorization: 'Token ' + localStorage.getItem('token')}
-          } else {
-            headers = {}
-          }
-          axios.get(this.url, {headers: headers})
-              .then(res => {
-                this.study_count = res.data.studies.data.count;
-                this.intervention_count = res.data.interventions.data.count;
-                this.group_count = res.data.groups.data.count;
-                this.individual_count = res.data.individuals.data.count;
-                this.output_count = res.data.outputs.data.count;
-              })
-              .catch(err => {
-                console.log(err.response.data);
-                this.loading = false
-              })
-              .finally(() => this.loading = false);
-        },
-      },
+        this.queries[key] = value
+      }
+    },
+    getData() {
+      this.loading = true
+      let headers = {};
+      if (localStorage.getItem('token')) {
+        let headers = {Authorization: 'Token ' + localStorage.getItem('token')}
+      }
+      axios.get(this.url, {headers: headers})
+          .then(res => {
+            this.study_count = res.data.studies.data.count;
+            this.intervention_count = res.data.interventions.data.count;
+            this.group_count = res.data.groups.data.count;
+            this.individual_count = res.data.individuals.data.count;
+            this.output_count = res.data.outputs.data.count;
+          })
+          .catch(err => {
+            console.log(err.response.data);
+            this.loading = false
+          })
+          .finally(() => this.loading = false);
+    },
+  },
 
 
-      data () {
+  data() {
     return {
       study_count: 0,
       intervention_count: 0,
@@ -115,19 +112,18 @@
 
       otype: "pkdata",
       otype_single: "pkdata",
-      queries :{
-        studies__name__in:[],
+      queries: {
+        studies__name__in: [],
         studies__reference_name__in: [],
         interventions__substance_name__in: [],
-        interventions__route_name__in:[],
-        interventions__application_name__in:[],
-        interventions__form_name__in:[],
-
+        interventions__route_name__in: [],
+        interventions__application_name__in: [],
+        interventions__form_name__in: [],
       }
     }
-    }
+  }
 
-    }
+}
 
 </script>
 
