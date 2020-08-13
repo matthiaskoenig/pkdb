ccc2a98480b4c0339425c0552ae7686c1ea01d47
Matthias Koenig <konigmatt@googlemail.com>
2020-08-13 12:51:40 +0200

    update info node component

--- frontend/src/components/InfoNode.vue
+++ frontend/src/components/InfoNode.vue
@@ -9,46 +9,53 @@
       <v-list-item-content>
         <div class="overline mb-4"><json-button :resource_url="url()"></json-button> {{ data.ntype.toUpperCase() }} <span v-if="data.dtype != 'undefined'">({{ data.dtype.toUpperCase() }})</span></div>
         <v-list-item-title class="headline mb-1">{{ data.label }}</v-list-item-title>
-        <v-list-item-subtitle>Parents: {{ data.parents.length>0 ? data.parents.join(', ') : "-" }}</v-list-item-subtitle>
+        <v-list-item-subtitle>Parents: {{ parents_labels.length>0 ? parents_labels.join(', ') : "-" }}</v-list-item-subtitle>
       </v-list-item-content>
-
-
     </v-list-item>
 
-    {{ data.description }}<br />
+    <div v-if="data.description & data.description.length>0">
+      {{ data.description }}<br />
+    </div>
 
-    <v-chip v-for="annotation in data.annotations" :key="annotation.term">
-        class="ma-1"
-        color="black"
-        dark
-        pill
-        small
-    >
-      {{annotation.relation}}|<strong>{{annotation.collection}}</strong>|{{ annotation.term }}
-    </v-chip>
-    <span v-if="annotation.label"><strong>{{annotation.label}}</strong></span> {{annotation.description ? annotation.description: ""}}<br />
-
-
-    <span class="label">Database links</span><br />
-    <span v-for="xref in data.xrefs" :key="xref.url">
+    <div v-if="data.annotations && data.annotations.length>0">
+      <span  v-for="annotation in data.annotations" :key="annotation.term">
       <v-chip
           class="ma-1"
           color="black"
-          outlined
+          dark
           pill
           small
-          :href="xref.url"
       >
-      <strong>{{ xref.name }}</strong>|{{ xref.accession}}
+        {{annotation.relation}}|<strong>{{annotation.collection}}</strong>|{{ annotation.term }}
       </v-chip>
-    </span>
-    <br />
-    <span class="label">Synonyms</span><br />
-    <ul>
-      <li v-for="synonym in data.synonyms" :key="synonym">
-        {{ synonym }}
-      </li>
-    </ul>
+
+      <span v-if="annotation.label"><strong>{{annotation.label}}</strong></span> {{annotation.description ? annotation.description: ""}}<br />
+      </span>
+    </div>
+
+    <div v-if="data.xrefs && data.xrefs.length>0">
+      <span class="label">Database links</span><br />
+        <v-chip  v-for="xref in data.xrefs" :key="xref.url"
+            class="ma-1"
+            color="black"
+            outlined
+            pill
+            small
+            :href="xref.url"
+        >
+        <strong>{{ xref.name }}</strong>|{{ xref.accession}}
+        </v-chip>
+      <br />
+    </div>
+
+    <div v-if="data.synonyms && data.synonyms.length>0">
+      <span class="label">Synonyms</span><br />
+      <ul>
+        <li v-for="synonym in data.synonyms" :key="synonym">
+          {{ synonym }}
+        </li>
+      </ul>
+    </div>
   </v-card>
 
 </template>
@@ -65,7 +72,18 @@ export default {
       exists: false,
     }
   },
-  computed: {},
+  computed: {
+    parents_labels: function () {
+      let labels = []
+      let parents = this.data.parents
+      if (parents){
+        for (let i=0; i<parents.length; i++ ){
+          labels.push(parents[i].label)
+        }
+      }
+      return labels
+    }
+  },
   methods: {
     url() {
       let entry_id = (this.$route.path).split('/').slice(-1)[0];
