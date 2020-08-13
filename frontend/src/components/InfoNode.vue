5dbfce6d3be863a18286c5a98d7c11871b7ce484
Matthias Koenig <konigmatt@googlemail.com>
2020-08-13 13:35:20 +0200

    updated info node component

--- frontend/src/components/InfoNode.vue
+++ frontend/src/components/InfoNode.vue
@@ -1,87 +1,19 @@
 <template>
-
-  <v-card v-if="exists"
-      class="mx-auto"
-      max-width="1000"
-      outlined
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="overline mb-4"><json-button :resource_url="url()"></json-button> {{ data.ntype.toUpperCase() }} <span v-if="data.dtype != 'undefined'">({{ data.dtype.toUpperCase() }})</span></div>
-        <v-list-item-title class="headline mb-1">{{ data.label }}</v-list-item-title>
-        <v-list-item-subtitle>Parents: {{ parents_labels.length>0 ? parents_labels.join(', ') : "-" }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <div v-if="data.description & data.description.length>0">
-      {{ data.description }}<br />
-    </div>
-
-    <div v-if="data.annotations && data.annotations.length>0">
-      <span  v-for="annotation in data.annotations" :key="annotation.term">
-      <v-chip
-          class="ma-1"
-          color="black"
-          dark
-          pill
-          small
-      >
-        {{annotation.relation}}|<strong>{{annotation.collection}}</strong>|{{ annotation.term }}
-      </v-chip>
-
-      <span v-if="annotation.label"><strong>{{annotation.label}}</strong></span> {{annotation.description ? annotation.description: ""}}<br />
-      </span>
-    </div>
-
-    <div v-if="data.xrefs && data.xrefs.length>0">
-      <span class="label">Database links</span><br />
-        <v-chip  v-for="xref in data.xrefs" :key="xref.url"
-            class="ma-1"
-            color="black"
-            outlined
-            pill
-            small
-            :href="xref.url"
-        >
-        <strong>{{ xref.name }}</strong>|{{ xref.accession}}
-        </v-chip>
-      <br />
-    </div>
-
-    <div v-if="data.synonyms && data.synonyms.length>0">
-      <span class="label">Synonyms</span><br />
-      <ul>
-        <li v-for="synonym in data.synonyms" :key="synonym">
-          {{ synonym }}
-        </li>
-      </ul>
-    </div>
-  </v-card>
-
+  <info-node-detail v-if="exists" :data="data" />
 </template>
 
 <script>
 import axios from 'axios'
+import InfoNodeDetail from './detail/InfoNodeDetail'
 
 export default {
-  name: 'InfoNode',
-  components: {},
+  components: {
+    InfoNodeDetail
+  },
   data() {
     return {
       data: null,
       exists: false,
-    }
-  },
-  computed: {
-    parents_labels: function () {
-      let labels = []
-      let parents = this.data.parents
-      if (parents){
-        for (let i=0; i<parents.length; i++ ){
-          labels.push(parents[i].label)
-        }
-      }
-      return labels
     }
   },
   methods: {
@@ -105,8 +37,4 @@ export default {
 }
 </script>
 <style>
-  .label {
-    font-weight: bold;
-    padding: 5px;
-  }
 </style>
