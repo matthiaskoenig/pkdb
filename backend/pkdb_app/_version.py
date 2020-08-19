33ae0e6ff0b19a5ff49df69de5094b76c80ada7c
Matthias Koenig <konigmatt@googlemail.com>
2020-08-19 22:42:51 +0200

    version bump for release

--- backend/pkdb_app/_version.py
+++ backend/pkdb_app/_version.py
@@ -1,4 +1,4 @@
 """
 Definition of version string.
 """
-__version__ = "0.9.0a2"
+__version__ = "0.9.0a3"
--- backend/pkdb_app/data/models.py
+++ backend/pkdb_app/data/models.py
@@ -166,7 +166,7 @@ class SubSet(Accessible):
                     name = self.get_name(timecourse[key], value)
                 else:
                     name = list(timecourse[key])
-                raise Exception(f"Subset used for timecourse is not unique on '{key}'. Values are {set(name)}. "
+                raise Exception(f"Subset used for timecourse is not unique on '{key}'. Values are {name}. "
                                 f"Check uniqueness of labels for timecourses.")
 
     def timecourse(self):
