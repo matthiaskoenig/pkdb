39e1dc0e02b0bc7f18b5c1210ef7870a9828454e
Matthias Koenig <konigmatt@googlemail.com>
2020-08-22 19:40:16 +0200

    cleaning up landing page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,39 +2,26 @@
   <div id="home">
     <v-row>
       <v-col>
-        <h2>PK-DB </h2>
-        <p></p>
-        <count-table/>
+        <overview/>
       </v-col>
     </v-row>
-
     <v-row>
       <v-col>
-        <about-information/>
+        <about/>
       </v-col>
     </v-row>
-
-    <!--
-    <v-row>
-      <v-col>
-      <statistics-vega-plot ></statistics-vega-plot>
-      </v-col>
-    </v-row>
-    -->
   </div>
 </template>
 
 <script>
-import CountTable from './tables/CountTable'
-import AboutInformation from './AboutInformation'
-import StatisticsVegaPlot from "./plots/StatisticsVegaPlot";
+import Overview from "./Overview";
+import About from './About'
 
 export default {
   name: 'Home',
   components: {
-    StatisticsVegaPlot,
-    CountTable,
-    AboutInformation,
+    Overview,
+    About,
   }
 }
 </script>
---
+++ frontend/src/components/Overview.vue
@@ -0,0 +1,151 @@
+<template>
+  <span id="overview">
+    <h1>PK-DB
+    <v-chip v-if="data.version"
+            flat
+            small
+            color="white"
+    >
+      Version: {{ data.version }}
+    </v-chip>
+    </h1>
+    <p>
+      PK-DB - an open database for pharmacokinetics information from clinical trials including pre-clinical research.
+      Data in PK-DB consists of outputs and timecourses linked to their respective study, group/individual and intervention.
+    </p>
+    <count-table v-bind="data"/>
+    <!--<statistics-vega-plot />-->
+    <v-row>
+      <v-col>
+    <v-card>
+      Search
+    </v-card>
+        </v-col>
+      <v-col>
+    <v-card>
+      Browse data
+    </v-card>
+        </v-col>
+    </v-row>
+
+  </span>
+
+
+</template>
+
+<script>
+import axios from 'axios'
+// import StatisticsVegaPlot from "./plots/StatisticsVegaPlot";
+import CountTable from "./tables/CountTable";
+
+export default {
+  name: "Overview",
+  components: {
+    // StatisticsVegaPlot,
+    CountTable,
+  },
+  data() {
+    return {
+      headers: [
+        { text: 'Count', value: 'count', sortable: false},
+        { text: 'Data', value: 'name', sortable: false},
+        { text: 'Description', value: 'description', sortable: false},
+      ],
+      data: {
+        version: "",
+        study_count: 0,
+        group_count: 0,
+        individual_count: 0,
+        intervention_count: 0,
+        output_count: 0,
+        timecourse_count: 0,
+        reference_count: 0,
+      },
+    }
+  },
+  computed: {
+    resource_url() {
+      return this.api + 'statistics/?format=json'
+    },
+    api() {
+      return this.$store.state.endpoints.api;
+    },
+    items() {
+      return [
+        {
+          name: 'Study',
+          name_plural: 'Studies',
+          to: '/studies',
+          icon: this.faIcon('studies'),
+          count: this.data.study_count,
+          description: "Clinical or experimental study measuring data in either single or multiple groups and/or single or multiple individuals."
+        },
+        {
+          name: 'Group',
+          to: '/groups',
+          icon: this.faIcon('groups'),
+          count: this.data.group_count,
+          description: "Group of individuals defined by certain characteristica, e.g., smoking status or medication."
+        },
+        {
+          name: 'Individual',
+          to: '/individuals',
+          icon: this.faIcon('individuals'),
+          count: this.data.individual_count,
+          description: "A single subject, characterized by the group it belongs to and personal characteristica like age, body weight or sex."
+        },
+        {
+          name: 'Intervention',
+          to: '/interventions',
+          icon: this.faIcon('interventions'),
+          count: this.data.intervention_count,
+          description: "Intervention which was performed in the study. Often this is the application of substances, e.g. caffeine or codeine, or changes in " +
+              "lifestyle like smoking cessation."
+        },
+        {
+          name: 'Output',
+          to: '/outputs',
+          icon: this.faIcon('outputs'),
+          count: this.data.output_count,
+          description: "Clinical or experimental output. These can be single parameters or variables, e.g. pharmacokinetic parameters like AUC, clearance or half-life of the applied substances."
+        },
+        {
+          name: 'Timecourse',
+          to: '/timecourses',
+          icon: this.faIcon('timecourses'),
+          count: this.data.timecourse_count,
+          description: "Clinical or experimental time course measurements."
+        },
+        {
+          name: 'Reference',
+          to: '/references',
+          icon: this.faIcon('references'),
+          count: this.data.reference_count,
+          description: "Literature references from which the data was digitized and curated."
+        },
+      ]
+    }
+  },
+  methods: {
+    fetch_data(url){
+      axios.get(url)
+          .then(response => {
+            this.data = response.data;
+          })
+          .catch((error)=>{
+            this.data = null;
+            console.error(this.resource_url);
+            console.error(error);
+            this.errors = error.response.data;
+          })
+    }
+  },
+  created() {
+    this.fetch_data(this.resource_url);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
