9fd7fee2b587b57d9c0e83019aea50b9c13a39ef
Matthias Koenig <konigmatt@googlemail.com>
2020-08-23 23:14:45 +0200

    Study Detail refactored

--- frontend/src/components/detail/StudyOverview.vue
+++ frontend/src/components/detail/StudyOverview.vue
@@ -1,108 +1,106 @@
 <template>
-  <!--
-  <v-card
-      width="100%"
-      flat
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="overline mb-4">
-          <json-button v-if="url" :resource_url="url"></json-button> {{ data.ntype.toUpperCase() }}
-          STUDY
-        </div>
-
-        <v-list-item-title class="headline mb-1"><text-highlight :queries="highlight">{{ data.label }}</text-highlight></v-list-item-title>
-        <v-list-item-subtitle v-if="parents_labels.length>0">Parents: {{ parents_labels.join(', ') }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-card-text>
-      <div v-if="data.description && data.description.length>0">
-        <text-highlight :queries="highlight">
-          {{ data.description }}<br />
-        </text-highlight>
-      </div>
-  -->
-
-
 
     <div class="study-info">
-        <v-layout wrap>
-            <v-flex md3>
-                <v-card flat>
-                <v-icon color="black">{{ faIcon('study') }}</v-icon>
-                <span class="heading-title">&nbsp;{{ study.name }} ({{ study.sid }})</span><br />
+      <v-card flat>
+      <v-row>
+        <v-col col="12">
+        <v-icon color="black">{{ faIcon('study') }}</v-icon>
+        <span class="heading-title">&nbsp;{{ study.name }} ({{ study.sid }})</span>
+          <v-btn icon>
+            <v-icon small
+                    :title="'Study is '+ study.access"
+                    :color="study.access =='public' ? 'green' : 'red'"
+            >{{ faIcon(study.access) }}</v-icon>
+          </v-btn>
 
-                <v-btn icon>
-                    <v-icon small
-                            :title="'Study is '+ study.access"
-                            :color="study.access =='public' ? 'green' : 'red'"
-                    >{{ faIcon(study.access) }}</v-icon>
-                </v-btn>
-                <v-btn icon>
-                    <v-icon small
-                            :title="'Publication is '+ study.licence + ' access'"
-                            :color="study.licence =='open' ? 'green' : 'red'"
-                    >{{ faIcon(study.licence) }}</v-icon>
-                </v-btn>
-                    {{ study.date }}<br />
+          <v-btn icon>
+            <v-icon small
+                    :title="'Publication is '+ study.licence + ' access'"
+                    :color="study.licence =='open' ? 'green' : 'red'"
+            >{{ faIcon(study.licence) }}</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
 
-                <Annotations :item="study"/>
-                </v-card>
-            </v-flex>
 
-            <v-flex md2>
-                <v-card flat>
-                    <div>
-                        <span class="attr">Creator</span><br />
-                        <user-avatar :user="study.creator"/>
-                    </div>
-                    <div>
-                        <span class="attr">Curators</span><br />
-                        <user-rating v-for="curator in study.curators"
-                                     :key="curator.username"
-                                     :user="curator"
-                        />
-                    </div>
-                     <div v-if="study.files.length > 0">
-                        <span class="attr">Files</span><br />
-                          <span v-for="file in study.files" :key="file.pk">
-                                <file-chip :file="file.file" />
-                        </span>
-                    </div>
-
-                </v-card>
-            </v-flex>
-            <v-flex md2>
-                <v-card flat>
-                    <div v-if="study.substances && study.substances.length!=0">
-                        <span class="attr">Substances</span><br />
-                        <span v-for="substance in study.substances" :key="substance.sid">
+        <v-row>
+          <v-col cols="4">
+        <div v-if="study.substances && study.substances.length!=0">
+          <span class="attr">Substances</span><br />
+          <span v-for="substance in study.substances" :key="substance.sid">
                             <object-chip :object="substance"
                                          otype="substance"
                             />
                         </span>
-                    </div>
-                </v-card>
-            </v-flex>
+        </div>
+          </v-col>
 
-            <v-flex md5>
-                <v-alert v-if="study.files.length == 0"
-                         dense
-                         text
-                         type="info"
-                >
-                    No images or no permission
-                </v-alert>
-                <file-image-view v-else :files="images"/>
-            </v-flex>
+          <v-col cols="4">
+            <div>
+              <span class="attr">Creator</span><br />
+              <user-avatar :user="study.creator"/>
+            </div>
+          </v-col>
+          <v-col cols="4">
 
-        </v-layout>
-        <v-layout wrap>
-            <v-flex md7>
-                        <reference-detail :reference="study.reference" :resource_url="reference_url(study.reference.sid)"/>
-            </v-flex>
-        </v-layout>
+          <span class="attr">Curators</span><br />
+          <user-rating v-for="curator in study.curators"
+                       :key="curator.username"
+                       :user="curator"
+          />
+          </v-col>
+        </v-row>
+
+      <v-row>
+        <v-col>
+        <reference-detail :reference="study.reference" :resource_url="reference_url(study.reference.sid)"/>
+        </v-col>
+      </v-row>
+
+        <v-row>
+          <v-col>
+          <Annotations :item="study"/>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+          <v-alert v-if="study.files.length == 0"
+                   dense
+                   text
+                   type="info"
+          >
+            No images or no permission
+          </v-alert>
+          <file-image-view v-else :files="images"/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div v-if="study.files.length > 0">
+              <span class="attr">Files</span><br />
+              <span v-for="file in study.files" :key="file.pk">
+                            <file-chip :file="file.file" />
+              </span>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+
+
+
+
+
+              <!--
+
+                -->
+
+
+
+
+
+
+
     </div>
 </template>
 
