67f7e9813e8e6a75650386e07afc6a44dce93750
Jan Grzegorzewski <janekg89@hotmail.de>
2020-09-03 16:22:24 +0200

    design

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -38,11 +38,25 @@
       Correlations between theses outputs are often shown in form of scatter plots.
 
     </p>
-    <img src="/assets/images/pkexample2.png" width="100%"/>
+    <count-table v-bind="data"/>
+
+    <v-img src="/assets/images/pkexample2.png" width="100%" class="mb-2">
+
+      <v-sheet  color="rgb(255,255,255, 0.9)"  rounded class="text-on-image text-caption text-right pl-2 pr-2" >
+        <strong> Curation Example</strong>
+        <v-spacer/>
+        RV Patwardhan, P V Desmond, R F Johnson, S Schenker
+        <v-spacer/>
+        <span class="font-italic text">
+          The Journal of Laboratory and clinical medicine, 1980-05-30
+        </span>
+
+
+      </v-sheet>
+    </v-img>
 
 
     <div>
-      <count-table v-bind="data"/>
 
     </div>
 
@@ -147,5 +161,12 @@
         </script>
 
         <style scoped>
+        .text-on-image {
+          margin-right: 0;
+          float: right;
+          position:absolute;
+          bottom: 0;
+          right: 0;
+        }
 
         </style>
