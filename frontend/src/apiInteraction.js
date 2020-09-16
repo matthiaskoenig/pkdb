20676af7ee49b706aea8e1abe72ffd6139223f34
Jan Grzegorzewski <janekg89@hotmail.de>
2020-09-16 21:52:29 +0200

    further refactoring

---
+++ frontend/src/apiInteraction.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import {StoreInteractionMixin} from "./storeInteraction";
+
+let ApiInteractionMixin = {
+    mixins: [StoreInteractionMixin],
+    methods: {
+        getStudy(sid) {
+            // object is an InfoNode
+            let url = `${this.$store.state.endpoints.api}studies/${sid}/?format=json`;
+            let headers = {};
+            if (localStorage.getItem('token')) {
+                headers = {Authorization: 'Token ' + localStorage.getItem('token')}
+            }
+            // get data (FIXME: caching of InfoNodes in store)
+            axios.get(url, {headers: headers})
+                .then(response => {
+                    if (response.data.sid) {
+                        this.updateSearch(response.data)
+                        this.show_type = "study";
+                        this.detail_info = response.data;
+                        this.display_detail = true;
+                    } else {
+                        this.$route.push('/404')
+                    }
+                })
+                .catch(err => {
+                    this.exists = false;
+                    this.$router.push('/404')
+                    console.log(err)
+                })
+                .finally(() => this.loading = false);
+        },
+    }
+}
+export {ApiInteractionMixin}
--- frontend/src/components/Data.vue
+++ frontend/src/components/Data.vue
@@ -9,12 +9,11 @@
 <script>
 import TableTabs from './tables/TableTabs';
 import SearchNavigation from './search/SearchNavigation'
-import axios from 'axios'
-import {StoreInteractionMixin} from "../storeInteraction";
+import {ApiInteractionMixin} from "../apiInteraction";
 
 export default {
   name: "Data",
-  mixins: [StoreInteractionMixin],
+  mixins: [ApiInteractionMixin],
   components: {
     SearchNavigation,
     TableTabs
@@ -27,7 +26,6 @@ export default {
     sid(){
       return this.$route.params.sid
     },
-
   },
   created() {
     if(this.sid){
@@ -37,35 +35,7 @@ export default {
       this.loading = false
     }
   },
-  methods: {
-    getStudy(sid) {
-      // object is an InfoNode
-      let url = `${this.$store.state.endpoints.api}studies/${sid}/?format=json`;
-      let headers = {};
-      if (localStorage.getItem('token')) {
-        headers = {Authorization: 'Token ' + localStorage.getItem('token')}
-      }
-      // get data (FIXME: caching of InfoNodes in store)
-      axios.get(url, {headers: headers})
-          .then(response => {
-            if(response.data.sid) {
-              this.updateSearch(response.data)
-              this.show_type = "study";
-              this.detail_info = response.data;
-              this.display_detail = true;
-            }else{
-              this.$route.push('/404')
-            }
-          })
-          .catch(err => {
-            this.exists = false;
-            this.$router.push('/404')
-            console.log(err)
-          })
-          .finally(() => this.loading = false);
-    },
-
-  }}
+  }
 </script>
 
 <style scoped>
